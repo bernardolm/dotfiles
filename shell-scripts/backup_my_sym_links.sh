ed7d13a331b89631494660725259cdbaf3f13428
Bernardo Loureiro <bernardo.lou@gmail.com>
2020-10-21 19:32:04 -0300

    Updating sym link backup

--- shell-scripts/backup_my_sym_links.sh
+++ shell-scripts/backup_my_sym_links.sh
@@ -1,22 +1,29 @@
-function backup_my_sym_links () {
-    find / -type l -ls | awk -F" " '{print "ln -sf " $13,$11}\' | grep '/home/'$USER \
-        | grep -iv '.oh-my-zsh/' \
-        | grep -iv '/.zinit/' \
-        | grep -iv '/app/' \
-        | grep -iv '/proc' \
-        | grep -iv '/tmp/' \
-        | grep -iv 'applications-merged' \
-        | grep -iv 'arduino' \
-        | grep -iv 'chrome' \
-        | grep -iv 'darwin' \
-        | grep -iv 'gnome-shell/extensions/' \
-        | grep -iv 'gopath' \
-        | grep -iv 'linux' \
-        | grep -iv 'node_modules' \
-        | grep -iv 'Rambox' \
-        | grep -iv 'snap' \
-        | grep -iv 'venv/' \
-        | grep -iv 'webkitgtk' \
-        | grep -iv $GITHUB_ORG \
-        >> $SYNC_PATH/scripts/bash-scripts/restore-my-sym-links.sh
+function backup_my_sym_links() {
+    find / -type l -ls | awk -F" " '{print "ln -sf " $13,$11}\' | grep '/home/'$USER |
+        grep -iv '.mozilla' |
+        grep -iv '.oh-my-zsh/' |
+        grep -iv '.wine' |
+        grep -iv '/.themes/' |
+        grep -iv '/.zinit/' |
+        grep -iv '/app/' |
+        grep -iv '/Dropbox/' |
+        grep -iv '/proc' |
+        grep -iv '/tmp/' |
+        grep -iv '/workspaces/' |
+        grep -iv 'applications-merged' |
+        grep -iv 'arduino' |
+        grep -iv 'chrome' |
+        grep -iv 'darwin' |
+        grep -iv 'genymotion' |
+        grep -iv 'gnome-shell/extensions/' |
+        grep -iv 'gopath' |
+        grep -iv 'linux' |
+        grep -iv 'lutris' |
+        grep -iv 'node_modules' |
+        grep -iv 'Rambox' |
+        grep -iv 'snap' |
+        grep -iv 'venv/' |
+        grep -iv 'webkitgtk' |
+        grep -iv $GITHUB_ORG \
+            >>$SYNC_PATH/scripts/bash-scripts/restore-my-sym-links.sh
 }
