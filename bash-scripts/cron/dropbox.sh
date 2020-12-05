22e9f42ea18b89bb3048ae2f7d0e99c8f9434cf3
Bernardo Loureiro <bernardo.lou@gmail.com>
2020-12-05 15:28:51 -0300

    Fixing user tmp path

--- bash-scripts/cron/dropbox.sh
+++ bash-scripts/cron/dropbox.sh
@@ -18,8 +18,8 @@ function cron/dropbox() {
     notify-send "dropbox crontab" "finish"
 }
 
-log_path="$USER_TMP/var/log/cron/dropbox"
-# log_path="$HOME/tmp/var/log/cron/dropbox"
+log_path="$USER_TMP/cron/dropbox"
+# log_path="$HOME/tmp/cron/dropbox"
 
 if [ ! -d "$log_path" ]; then
     echo -e "creating dropbox log folder '$log_path'..."
--- bash-scripts/cron/keep-mic-muted.sh
+++ bash-scripts/cron/keep-mic-muted.sh
@@ -21,8 +21,8 @@ function keep_mic_muted() {
     keep_mic_muted $1 $times
 }
 
-log_path="$USER_TMP/var/log/cron/keep-mic-muted"
-# log_path="$HOME/tmp/var/log/cron/keep-mic-muted"
+log_path="$USER_TMP/cron/keep-mic-muted"
+# log_path="$HOME/tmp/cron/keep-mic-muted"
 
 if [ ! -d "$log_path" ]; then
     echo -e "creating keep-mic-muted log folder '$log_path'..."
