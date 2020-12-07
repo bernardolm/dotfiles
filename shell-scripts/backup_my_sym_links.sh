8a898dcd756a883f0e1df7fa5d524202533068ef
Bernardo Loureiro <bernardo.lou@gmail.com>
2020-12-07 00:39:38 -0300

    Adding more smart my sym link restore

--- shell-scripts/backup_my_sym_links.sh
+++ shell-scripts/backup_my_sym_links.sh
@@ -1,5 +1,5 @@
 function backup_my_sym_links() {
-    find / -type l -ls 2>/dev/null | awk -F" " '{print "/bin/rm "$11" && ln -sf " $13,$11}\' | grep '/home/'$USER |
+    find / -type l -ls 2>/dev/null | awk -F" " '{print $13";"$11}\' | grep '/home/'$USER |
         grep -iv '.mozilla' |
         grep -iv '.oh-my-zsh/' |
         grep -iv '.wine' |
@@ -24,5 +24,5 @@ function backup_my_sym_links() {
         grep -iv 'venv/' |
         grep -iv 'webkitgtk' |
         grep -iv $GITHUB_ORG \
-            >>$SYNC_PATH/scripts/bash-scripts/restore-my-sym-links.sh
+            >>$SYNC_PATH/scripts/my-sym-links.txt
 }
