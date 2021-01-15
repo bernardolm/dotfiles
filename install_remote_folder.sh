57bbf6b94d13b5d88e8b9032db18e04efdcc3b32
Bernardo Loureiro <bernardo.lou@gmail.com>
2021-01-15 00:06:55 -0300

    Fixing dropbox install

--- install_remote_folder.sh
+++ install_remote_folder.sh
@@ -5,31 +5,32 @@ source $BASE_PATH/msg.sh
 msg_init 'remote folder'
 
 function install_dropbox() {
+    function is_dropbox_finish() {
+        check1=`dropbox status | grep -c -i "up to date" | bc`
+        check2=`dropbox status | grep -c -i "atualizado" | bc`
+        result=`expr $check1 + $check2`
+        [ $result -gt 0 ] && true
+    }
+
     if [[ "$(command -v dropbox)" == "" ]]; then
         echo -e "\n💾 installing dropbox..."
         sudo apt install python3-gpg
         nautilus --quit
         curl https://linux.dropbox.com/packages/ubuntu/dropbox_2020.03.04_amd64.deb -o ~/tmp/dropbox.deb && sudo dpkg -i ~/tmp/dropbox.deb
     fi
-}
 
-function is_dropbox_finish() {
-    check1=`dropbox status | grep -c -i "up to date" | bc`
-    check2=`dropbox status | grep -c -i "atualizado" | bc`
-    result=`expr $check1 + $check2`
-    [ $result -gt 0 ] && true
+    dropbox start &> /dev/null
+
+    while : ; do
+        is_dropbox_finish && break
+        dropbox status
+        sleep 5s
+    done
+
+    [ ! -h ~/Sync ] && ln -s ~/Dropbox ~/Sync
+    export SYNC_PATH="~/Sync/config-backup"
 }
 
 install_dropbox
-dropbox start &> /dev/null
-
-while : ; do
-    is_dropbox_finish && break
-    dropbox status
-    sleep 5s
-done
-
-[ ! -h ~/Sync ] && ln -s ~/Dropbox ~/Sync
-export SYNC_PATH="~/Sync/config-backup"
 
 msg_end 'remote folder'
