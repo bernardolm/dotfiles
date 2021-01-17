77cba2e89ad9e5f5815616a8ea3cde6be6378320
Bernardo Loureiro <bernardo.lou@gmail.com>
2021-01-17 12:44:25 -0300

    Splitting post install

--- first-steps-ubuntu.sh
+++ first-steps-ubuntu.sh
@@ -19,6 +19,7 @@ msg_welcome
 
 ## Upgrade ubuntu
 msg_init 'ubuntu upgrade'
+sudo apt update
 sudo apt upgrade --yes
 msg_end 'ubuntu upgrade'
 
@@ -33,24 +34,6 @@ $BASE_PATH/install_base_packages.sh
 $BASE_PATH/install_3rd_party_packages.sh
 $BASE_PATH/install_3rd_party_apps.sh
 $BASE_PATH/install_snap_packages.sh
-
-# post install
-msg_init 'running post install commands'
-sudo apt --purge autoremove
-chsh -s $(which zsh)
-fc-cache -f -v > /dev/null
-gnome-extensions disable ubuntu-dock@ubuntu.com
-# pip3 install -U --user pygments
-echo -e "vm.swappiness=0" | sudo tee -a /etc/sysctl.conf
-echo -e "#\x21/bin/sh\\nfstrim -v /" | sudo tee /etc/cron.daily/trim
-sudo chmod +x /etc/cron.daily/trim
-# add noatime,nodiratime flag to /etc/fstab
-sudo sysctl -w net.ipv6.conf.all.disable_ipv6=1
-sudo sysctl -w net.ipv6.conf.default.disable_ipv6=1
-sudo sysctl -w net.ipv6.conf.lo.disable_ipv6=1
-
-## Reload gconfs
-gconftool-2 --shutdown
-gconftool-2 --spawn
+$BASE_PATH/post-install.sh.sh
 
 echo -e '\n\n🏁 thats all folks... bye!'
---
+++ post-install.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+source $BASE_PATH/msg.sh
+
+msg_init 'post install'
+
+sudo apt --purge autoremove
+chsh -s $(which zsh)
+fc-cache -f -v > /dev/null
+gnome-extensions disable ubuntu-dock@ubuntu.com
+
+sudo sysctl -w net.ipv6.conf.all.disable_ipv6=1
+sudo sysctl -w net.ipv6.conf.default.disable_ipv6=1
+sudo sysctl -w net.ipv6.conf.lo.disable_ipv6=1
+
+gconftool-2 --shutdown
+gconftool-2 --spawn
+
+msg_end 'post install'
