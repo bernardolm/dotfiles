d3266a0708399e1ff67e654183b7c90a86328e37
Bernardo Loureiro <bernardo.lou@gmail.com>
2021-01-18 01:18:57 -0300

    Fixing export

--- first-steps-ubuntu.sh
+++ first-steps-ubuntu.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 set -e
+set -a
 
 export BASE_PATH="$( cd "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 
--- install-3rd-party-apps.sh
+++ install-3rd-party-apps.sh
@@ -4,6 +4,8 @@ source $BASE_PATH/msg.sh
 
 msg_init '3rd party apps'
 
+export PATH=$PATH:~/.local/bin
+
 for NAME in $(find $(dirname $(readlink -f $0))/3rd-party-apps/*.sh); do
     source $NAME
 done
