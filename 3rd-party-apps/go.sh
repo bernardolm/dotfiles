a5fb4af78832684b467c5c0cff683e7da7ee4796
Bernardo Loureiro <bernardo.lou@gmail.com>
2021-01-17 21:57:01 -0300

    Fixing golang install

--- 3rd-party-apps/go.sh
+++ 3rd-party-apps/go.sh
@@ -5,9 +5,9 @@ source $BASE_PATH/msg.sh
 msg_init 'go'
 
 if [[ "$(command -v go)" == "" ]]; then
-    wget --quiet https://golang.org/dl/go1.15.2.linux-amd64.tar.gz -O ~/tmp/go.tar.gz
-    sudo /bin/rm -rf $GOPATH/pkg
-    sudo tar -C /usr/local -xzf ~/tmp/go.tar.gz
+    wget --quiet https://golang.org/dl/go1.15.6.linux-amd64.tar.gz -O ~/tmp/go.tar.gz
+    sudo tar -vC /usr/local -xzf ~/tmp/go.tar.gz
+    export PATH=$PATH:/usr/local/go/bin
 fi
 
 msg_end 'go'
