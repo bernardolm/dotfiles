440fdf03bfd1c8e5d09e2825555a0f64f319d244
Bernardo Loureiro <bernardo.lou@gmail.com>
2021-01-18 00:26:37 -0300

    Fixing PATH

--- 3rd-party-apps/todo.txt-cli.sh
+++ 3rd-party-apps/todo.txt-cli.sh
@@ -9,6 +9,7 @@ if [[ "$(command -v todo.sh)" == "" ]]; then
     curl -s -L https://github.com/todotxt/todo.txt-cli/releases/download/v$version/todo.txt_cli-$version.zip -o ~/tmp/todo.zip
     unzip ~/tmp/todo.zip -d ~/tmp/todo
     yes | mv ~/tmp/todo/todo.txt_cli-$version.dirty/todo.sh ~/.local/bin
+    export PATH:$PATH:~/.local/bin
 fi
 
 msg_end 'todo.txt-cli'
