a29773630e88f87fdc42c6c103948f31e7497acd
Bernardo Loureiro <bernardo.lou@gmail.com>
2021-01-17 23:12:34 -0300

    Fixing guake install

--- 3rd-party-apps/guake.sh
+++ 3rd-party-apps/guake.sh
@@ -6,6 +6,7 @@ msg_init 'guake'
 
 if [[ "$(command -v guake)" == "" ]]; then
     # curl -L -o ~/tmp/guake.zip https://github.com/Guake/guake/archive/master.zip
+    rm -rf ~/tmp/guake
     git clone https://github.com/Guake/guake.git ~/tmp/guake
     # unzip -oq ~/tmp/guake.zip -d ~/tmp/guake
     cd ~/tmp/guake/guake-master
