df1f2e17beca94825e4ec609e3eb6a4581434bda
Bernardo Loureiro <bernardo.lou@gmail.com>
2021-01-17 23:09:48 -0300

    Checking for existent keys

--- setting-ssh.sh
+++ setting-ssh.sh
@@ -4,8 +4,12 @@ source $BASE_PATH/msg.sh
 
 msg_init 'setting ssh'
 
-ssh-keygen -t ed25519 -C "`git config user.email`"
-eval "$(ssh-agent -s)"
-ssh-add ~/.ssh/id_ed25519
+if [ `ls -al ~/.ssh | grep -c id_ | bc` == 0 ]; then
+    ssh-keygen -t ed25519 -C "`git config user.email`"
+    eval "$(ssh-agent -s)"
+    ssh-add ~/.ssh/id_ed25519
+else
+    echo "keys founded"
+fi
 
 msg_end 'setting ssh'
