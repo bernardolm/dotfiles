5819adacd7948cb5b95179f9d45effeb8c0d879f
Bernardo Loureiro <bernardo.lou@gmail.com>
2021-01-19 15:37:57 -0300

    Calling only last call is old

--- setting-apt.sh
+++ setting-apt.sh
@@ -4,13 +4,37 @@ source $BASE_PATH/msg.sh
 
 msg_init 'setting apt'
 
-source $BASE_PATH/shell-scripts/get_keys.sh
-
-get_keys
-
 sudo dpkg --add-architecture i386
 
-sudo apt update
-sudo apt upgrade --yes
+[ -f ~/tmp/get_keys ] || touch -d "2 years ago" ~/tmp/get_keys
+
+if test `find ~/tmp/get_keys -mmin +180`; then
+    echo "fixing absent keys..."
+    source $BASE_PATH/shell-scripts/get_keys.sh
+    get_keys
+    touch ~/tmp/get_keys
+else
+    echo "keys already fixed"
+fi
+
+[ -f ~/tmp/sudo_apt_update ] || touch -d "2 years ago" ~/tmp/sudo_apt_update
+
+if test `find ~/tmp/sudo_apt_update -mmin +180`; then
+    echo "apt updating..."
+    sudo apt update
+    touch ~/tmp/sudo_apt_update
+else
+    echo "apt already updated"
+fi
+
+[ -f ~/tmp/sudo_apt_upgrade ] || touch -d "2 years ago" ~/tmp/sudo_apt_upgrade
+
+if test `find ~/tmp/sudo_apt_upgrade -mmin +180`; then
+    echo "apt updating..."
+    sudo apt upgrade --yes
+    touch ~/tmp/sudo_apt_upgrade
+else
+    echo "apt already upgraded"
+fi
 
 msg_end 'setting apt'
