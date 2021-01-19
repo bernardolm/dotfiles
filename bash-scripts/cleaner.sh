9b0ac6f5f23385d615e84afc77a7c7d09b24bdd2
Bernardo Loureiro <bernardo.lou@gmail.com>
2021-01-19 13:24:27 -0300

    Fixing tmp cleaner

--- bash-scripts/cleaner.sh
+++ bash-scripts/cleaner.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-setopt +o nomatch &&
-    /bin/rm -rf ~/tmp/git-clone/* &&
-    /bin/rm -rf ~/tmp/git-update/* &&
-    /bin/rm -rf ~/tmp/cron/dropbox/* &&
+[ `command -v setopt` ] && setopt +o nomatch
+/bin/rm -rf ~/tmp/git-clone/* && \
+    /bin/rm -rf ~/tmp/git-update/* && \
+    /bin/rm -rf ~/tmp/cron/dropbox/* && \
     /bin/rm -rf ~/tmp/cron/keep-mic-muted/*
