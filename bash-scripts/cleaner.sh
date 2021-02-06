ce41a47b333ebba6e7d685db4b7b4fbe08ec8aae
Bernardo Loureiro <bernardo.lou@gmail.com>
2021-02-06 14:25:11 -0300

    Removing not used script

--- bash-scripts/cleaner.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-[ `command -v setopt` ] && setopt +o nomatch
-/bin/rm -rf ~/tmp/git-clone/* && \
-    /bin/rm -rf ~/tmp/git-update/* && \
-    /bin/rm -rf ~/tmp/cron/dropbox/* && \
-    /bin/rm -rf ~/tmp/cron/keep-mic-muted/*
