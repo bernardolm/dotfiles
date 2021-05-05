71405a9adcb0095f00b62ca03ca87004970631eb
Bernardo Loureiro <bernardo.lou@gmail.com>
2021-05-05 11:19:11 -0300

    Fixing apt package search

--- shell-scripts/apt_installed_packages_with_text.sh
+++ shell-scripts/apt_installed_packages_with_text.sh
@@ -1,3 +1,3 @@
 function apt_installed_packages_with_text() {
-    apt-get list --installed "*$1*" 2>/dev/null | awk -F'/' 'NR>1{print $1}'
+    apt list --installed "*$1*" 2>/dev/null | awk -F'/' 'NR>1{print $1}'
 }
