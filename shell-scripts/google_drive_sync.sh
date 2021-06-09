1fccd546957c3e69a152dc6e941d70c3498dcc3f
Bernardo Loureiro <bernardo.lou@gmail.com>
2021-06-09 10:12:34 -0300

    Fixing google drive command

--- shell-scripts/google_drive_sync.sh
+++ shell-scripts/google_drive_sync.sh
@@ -6,7 +6,7 @@ function google_drive_sync() {
     rsync -avu --delete $SYNC_PATH/ $HOME/google-drive/config-backup/
 
     echo -e "syncing virtual..."
-    drive push $HOME/google-drive/config-backup/
+    drive push -ignore-name-clashes -ignore-conflict $HOME/google-drive
 
     echo -e "finish google drive worker"
     notify-send "google drive worker" "finish"
