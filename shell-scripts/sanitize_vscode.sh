54f039d434a3397bb51cbbd3a9ecc16f9ef64ac8
Bernardo Loureiro <bernardo.lou@gmail.com>
2021-06-09 10:13:31 -0300

    Updating vccode sanitize

--- shell-scripts/sanitize_vscode.sh
+++ shell-scripts/sanitize_vscode.sh
@@ -3,7 +3,12 @@ function sanitize_vscode() {
     /bin/rm -rf ~/.config/Code/blob_storage
     /bin/rm -rf ~/.config/Code/Cache
     /bin/rm -rf ~/.config/Code/CachedData
+    /bin/rm -rf ~/.config/Code/Code\ Cache
     /bin/rm -rf ~/.config/Code/GPUCache
     /bin/rm -rf ~/.config/Code/logs
+    /bin/rm -rf ~/.config/Code/Session\ Storage
     /bin/rm -rf ~/.config/Code/webrtc_event_logs
+    /bin/rm -rf ~/.vscode/Code/Crash\ Reports
+    /bin/rm -rf ~/.vscode/Code/exthost\ Crash\ Reports
+    /bin/rm -rf ~/.vscode/Code/Local\ Storage
 }
