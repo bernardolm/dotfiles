07d9a8b7a5042f95b9d36110092c18c5394aca92
Bernardo Loureiro <bernardo.lou@gmail.com>
2021-07-12 17:29:26 -0300

    Removing vendor files from google sync

--- shell-scripts/google_drive_sync.sh
+++ shell-scripts/google_drive_sync.sh
@@ -3,13 +3,15 @@ function google_drive_sync() {
     notify-send "google drive worker" "running..."
 
     echo -e "syncing local..."
-    rsync -avu --delete $SYNC_PATH/ $HOME/google-drive/config-backup/
+    rsync -au --delete $SYNC_PATH/ $HOME/google-drive/config-backup/
+
+    echo -e "removing vendor paths..."
+    find $HOME/google-drive/config-backup/ -name 'node_modules' -exec /bin/rm rm -rf {} \; 2>/dev/null
 
     echo -e "syncing virtual..."
-    time (cd $HOME/google-drive && \
-        exec drive push -ignore-name-clashes -ignore-conflict config-backup && \
-        exec drive pull -ignore-name-clashes -ignore-conflict config-backup
-    )
+    cd $HOME/google-drive && \
+        drive pull -ignore-name-clashes -ignore-conflict config-backup && \
+        drive push -ignore-name-clashes -ignore-conflict config-backup
 
     echo -e "finish google drive worker"
     notify-send "google drive worker" "finish"
