66ac4480410e03f23034165b7931997525e0cdea
Bernardo Loureiro <bernardo.lou@gmail.com>
2021-07-22 12:15:57 -0300

    Using rclone to google drive sync

--- shell-scripts/google_drive_sync.sh
+++ shell-scripts/google_drive_sync.sh
@@ -6,12 +6,13 @@ function google_drive_sync() {
     rsync -au --delete $SYNC_PATH/ $HOME/google-drive/config-backup/
 
     echo -e "removing vendor paths..."
-    find $HOME/google-drive/config-backup/ -name 'node_modules' -exec /bin/rm rm -rf {} \; 2>/dev/null
+    find $HOME/google-drive/config-backup/ -name 'node_modules' \
+        -exec /bin/rm rm -rf {} \; 2>/dev/null
 
     echo -e "syncing virtual..."
-    cd $HOME/google-drive && \
-        drive pull -ignore-name-clashes -ignore-conflict config-backup && \
-        drive push -ignore-name-clashes -ignore-conflict config-backup
+    rclone sync --check-first --delete-before --delete-excluded --fast-list \
+        --progress --rc --rc-enable-metrics --transfers 12 \
+        $HOME/google-drive/config-backup/ config-backup:
 
     echo -e "finish google drive worker"
     notify-send "google drive worker" "finish"
