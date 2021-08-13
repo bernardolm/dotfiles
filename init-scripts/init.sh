c91cbc3a47753334cc9db86ebfd3031f44001c79
Bernardo Loureiro <bernardo.lou@gmail.com>
2021-08-13 18:09:31 -0300

    Refactoring zinit starter

--- init-scripts/init.sh
+++ init-scripts/init.sh
@@ -10,23 +10,23 @@ export GITHUB_ORG=$(git config github.organization)
 source $HOME/workspaces/$GITHUB_USER/first-steps-ubuntu/init-scripts/workspace.sh
 
 # Loading init scripts
-INIT_PATHS=(
+init_scripts_path=(
     $SYNC_PATH/scripts/init-scripts
     $WORKSPACE_USER/first-steps-ubuntu/init-scripts
 )
 
-$DEBUG && echo "💿 loading init scripts in paths $INIT_PATHS"
+$DEBUG && echo "💿 loading init scripts in paths $init_scripts_path"
 
-for P in $INIT_PATHS; do
-    $DEBUG && echo "📁 loading path $P"
-    if [ ! -d "$P" ]; then
-        $DEBUG && echo "\t📄 loading script `basename $P`"
-        source $p
+for scripts_path in $init_scripts_path; do
+    $DEBUG && echo "📁 loading path $scripts_path"
+    if [ ! -d "$scripts_path" ]; then
+        $DEBUG && echo "\t📄 loading script `basename $scripts_path`"
+        source $scripts_path
     else
-        $DEBUG && echo "\t🔎 searching scripts in path `basename $P`"
-        for SP in $(find $P/*.sh ! -name '*init.sh'); do
-            $DEBUG && echo "\t\t📄 loading script `basename $SP`"
-            source $SP
+        $DEBUG && echo "\t🔎 searching scripts in path `basename $scripts_path`"
+        for another_scripts_path in $(find $scripts_path/*.sh ! -name '*init.sh'); do
+            $DEBUG && echo "\t\t📄 loading script `basename $another_scripts_path`"
+            source $another_scripts_path
         done
     fi
 done
