f0d8f255c06da78fdec3b9945cd6031cc312c6cd
Bernardo Loureiro <bernardo.lou@gmail.com>
2021-09-10 15:07:04 -0300

    Fixing global envs

--- init-scripts/env.sh
+++ init-scripts/env.sh
@@ -8,22 +8,22 @@ export USER_TMP=$HOME/tmp
 export GITHUB_USER=$(git config github.user)
 export GITHUB_ORG=$(git config github.organization)
 
-[ -d ~/workspaces/$GITHUB_USER ] && export WORKSPACE_USER=~/workspaces/$GITHUB_USER
-[ -d ~/workspaces/$GITHUB_ORG ] && export WORKSPACE_ORG=~/workspaces/$GITHUB_ORG
+[ ! -z $GITHUB_ORG ] && export GOPRIVATE=github.com/$GITHUB_ORG/*
 
-[ -z $WORKSPACE_USER ] && export DOTFILES=$WORKSPACE_USER/dotfiles
+[ -d ~/workspaces/$GITHUB_USER ] && export WORKSPACE_USER=~/workspaces/$GITHUB_USER
+[ ! -z $WORKSPACE_USER ] && export DOTFILES=$WORKSPACE_USER/dotfiles
+
+[ -d ~/workspaces/$GITHUB_ORG ] && export WORKSPACE_ORG=~/workspaces/$GITHUB_ORG
 
 [ ! -d $WORKSPACE_USER ] && mkdir -p $WORKSPACE_USER
 [ ! -d $WORKSPACE_ORG ] && mkdir -p $WORKSPACE_ORG
 
 [ -d /usr/local/go/bin ] && export PATH=$PATH:/usr/local/go/bin:
-[ -z $GITHUB_ORG ] && export GOPRIVATE=github.com/$GITHUB_ORG/*
+
 [ -d ~/gopath ] && export GOPATH=~/gopath
 [ -d $GOPATH/bin ] && export PATH=$PATH:$GOPATH/bin:
 
-[ -f $SYNC_PATH/aliases ] && \
-    (($DEBUG && echo "loading sync path aliases") || true) && \
+[ -f $SYNC_PATH/aliases ] && (($DEBUG && echo "loading sync path aliases") || true) && \
     source $SYNC_PATH/aliases
-[ -f $DOTFILES/aliases ] && \
-    (($DEBUG && echo "loading git path aliases") || true) && \
+[ -f $DOTFILES/aliases ] && (($DEBUG && echo "loading git path aliases") || true) && \
     source $DOTFILES/aliases
