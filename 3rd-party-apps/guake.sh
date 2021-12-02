a128e1d5d798aa1216dfe0f753d62b7675ce4060
Bernardo Loureiro <bernardo.lou@gmail.com>
2021-12-02 15:13:07 -0300

    Updating guake install commands

--- 3rd-party-apps/guake.sh
+++ 3rd-party-apps/guake.sh
@@ -5,8 +5,15 @@ source $BASE_PATH/msg.sh
 msg_init 'guake'
 
 if [[ "$(command -v guake)" == "" ]]; then
-    pip3 install --user guake
-    export PATH=$PATH:$HOME/.local/bin:
+    # pip3 install --user guake
+    # export PATH=$PATH:$HOME/.local/bin:
+    # /usr/local/bin/guake
+
+    git clone https://github.com/Guake/guake.git ~/.guake
+    cd ~/.guake
+    ./scripts/bootstrap-dev-debian.sh run make
+    make
+    sudo make install
 fi
 
 msg_end 'guake'
