8d5ba9805970dfa04069796898a9fd8e61e26098
Bernardo Loureiro <bernardo.lou@gmail.com>
2022-02-17 16:55:21 -0300

    Fixing file checks

--- shell/crontab.sh
+++ shell/crontab.sh
@@ -1,5 +1,6 @@
 function backup_crontab() {
     mkdir -p $SYNC_PATH/crontab/
-    mv $SYNC_PATH/crontab/$(hostname)_current.txt $SYNC_PATH/crontab/$(hostname)_$(date +"%Y%m%d%H%M%S").txt
+    [ -f $SYNC_PATH/crontab/$(hostname)_current.txt ] && mv \
+        $SYNC_PATH/crontab/$(hostname)_current.txt $SYNC_PATH/crontab/$(hostname)_$(date +"%Y%m%d%H%M%S").txt
     crontab -l > $SYNC_PATH/crontab/$(hostname)_current.txt
 }
