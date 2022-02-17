a8c4a26d54c94f7689b1d206cfc3ebef82450d54
Bernardo Loureiro <bernardo.lou@gmail.com>
2022-02-17 17:54:18 -0300

    Fixing snap func

--- shell/snap_packages.sh
+++ shell/snap_packages.sh
@@ -2,7 +2,7 @@ function backup_snap_packages() {
     mkdir -p $SYNC_PATH/snap-packages/
     [ -f $SYNC_PATH/snap-packages/$(hostname)_current.txt ] && mv \
         $SYNC_PATH/snap-packages/$(hostname)_current.txt $SYNC_PATH/snap-packages/$(hostname)_$(date +"%Y%m%d%H%M%S").txt
-    snap list > $SYNC_PATH/snap-packages/$(hostname)_current.txt
+    snap list | /bin/grep -v Publisher > $SYNC_PATH/snap-packages/$(hostname)_current.txt
 }
 
 function restore_snap_packages() {
@@ -12,10 +12,10 @@ function restore_snap_packages() {
     sudo snap refresh
 
     echo "installing classic snap packages..."
-    /bin/cat $file | /bin/grep -v Publisher | /bin/grep classic | /bin/awk -F' ' '{print $1}' | \
+    /bin/cat $file | /bin/grep classic | /bin/awk -F' ' '{print $1}' | \
         sort | uniq | xargs -L1 sudo snap install --classic
 
     echo "installing snap packages..."
-    /bin/cat $file | /bin/grep -v Publisher | /bin/grep -v classic | /bin/awk -F' ' '{print $1}' | \
+    /bin/cat $file | /bin/grep -v classic | /bin/awk -F' ' '{print $1}' | \
         sort | uniq | tr '\n' ' ' | xargs sudo snap install
 }
