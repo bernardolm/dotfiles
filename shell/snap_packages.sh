e6830b560923a0cff7aa15556e51ecf0820fd553
Bernardo Loureiro <bernardo.lou@gmail.com>
2022-02-17 16:38:19 -0300

    Refactoring snap backup and restore

--- shell/snap_packages.sh
+++ shell/snap_packages.sh
@@ -1,6 +1,7 @@
 function backup_snap_packages() {
     mkdir -p $SYNC_PATH/snap-packages/
-    mv $SYNC_PATH/snap-packages/$(hostname)_current.txt $SYNC_PATH/snap-packages/$(hostname)_$(date +"%Y%m%d%H%M%S").txt
+    [ -f $SYNC_PATH/snap-packages/$(hostname)_current.txt ] && mv \
+        $SYNC_PATH/snap-packages/$(hostname)_current.txt $SYNC_PATH/snap-packages/$(hostname)_$(date +"%Y%m%d%H%M%S").txt
     snap list > $SYNC_PATH/snap-packages/$(hostname)_current.txt
 }
 
@@ -8,10 +9,13 @@ function restore_snap_packages() {
     local 'file'
     local 'packages'
     local 'packages_classic'
-    file=$(last_backup_version snap-packages)
+    file=$(last_backup_version snap-packages txt)
+
     sudo snap refresh
-    echo "installing classic snap packages"
-    cat $file | grep -v Publisher | grep classic | cut -d' ' -f1 | xargs -n1 -d'\n' sudo snap install --classic
-    echo "installing snap packages"
-    cat $file | grep -v Publisher | grep -v classic | cut -d' ' -f1 | xargs -n1 -d'\n' sudo snap install
+
+    echo "installing classic snap packages..."
+    /bin/cat $file | /bin/grep -v Publisher | /bin/grep classic | /bin/awk -F' ' '{print $1}' ORS=' ' | xargs sudo snap install --classic
+
+    echo "installing snap packages..."
+    /bin/cat $file | /bin/grep -v Publisher | /bin/grep -v classic | /bin/awk -F' ' '{print $1}' ORS=' ' | xargs sudo snap install
 }
