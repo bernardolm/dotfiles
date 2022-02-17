cf22cf692343d8597bf5cd1b1f0c5aab658e5779
Bernardo Loureiro <bernardo.lou@gmail.com>
2022-02-17 16:37:24 -0300

    Refactoring last backup version

--- shell/last_backup_version.sh
+++ shell/last_backup_version.sh
@@ -2,25 +2,22 @@ function last_backup_version() {
     local 'module'
     local 'path'
     local 'file'
-    local 'file_fallback'
 
     module=$1
     [ -z "$module" ] && echo "you needs to specify a module to retrieve last backup version" && return
 
+    ext=$2
+    [ -z "$ext" ] && echo "you needs to specify a extension to retrieve last backup version" && return
+
     path=$SYNC_PATH/$module
     [ ! -d "$path" ] && echo "'$path' isn't a valid module path to retrieve last backup version" && return
 
-    file="$path/${hostname}_current.txt"
-
-    if [ -f "$file" ]; then
-        echo $file
-    fi
+    file="$path/${hostname}_current.${ext}"
+    [ -f "$file" ] && echo $file && return
 
     file=$path/$(/bin/ls $path | /bin/grep current | /bin/head -n1)
+    [ -f "$file" ] && echo $file && return
 
-    if [ -f "$file" ]; then
-        echo $file
-    fi
-
-    return 1
+    file="$path/current.${ext}"
+    [ -f "$file" ] && echo $file && return
 }
