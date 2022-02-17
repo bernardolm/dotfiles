b73874123d0146454aa6983a15c7a5e647e22219
Bernardo Loureiro <bernardo.lou@gmail.com>
2022-02-17 16:38:01 -0300

    Refactoring python packages backup

--- shell/python_packages.sh
+++ shell/python_packages.sh
@@ -1,9 +1,12 @@
 function backup_python_packages() {
     mkdir -p $SYNC_PATH/python-packages/
     mv $SYNC_PATH/python-packages/$(hostname)_current.txt $SYNC_PATH/python-packages/$(hostname)_$(date +"%Y%m%d%H%M%S").txt
-    pip freeze | grep -v 'git@' > $SYNC_PATH/python-packages/$(hostname)_current.txt
+    pip freeze | grep '==' > $SYNC_PATH/python-packages/$(hostname)_current.txt.tmp
 }
 
 function restore_python_packages() {
-    /bin/cat $SYNC_PATH/python_packages/$(hostname)_current.txt | grep -v git@ | xargs pip install
+    local 'file'
+    file=$(last_backup_version python-packages txt)
+
+    /bin/cat $file | /bin/awk -F'==' '{print $1}' ORS=' ' | xargs pip3 install
 }
