d472a59d2c00cf9ec03078eddc5004b0482b042f
Bernardo Loureiro <bernardo.lou@gmail.com>
2022-02-17 16:36:31 -0300

    Adding restore apt packages

--- shell/apt_packages.sh
+++ shell/apt_packages.sh
@@ -3,3 +3,16 @@ function backup_apt_packages() {
     mv $SYNC_PATH/apt-packages/$(hostname)_current.txt $SYNC_PATH/apt-packages/$(hostname)_$(date +"%Y%m%d%H%M%S").txt
     dpkg --get-selections > $SYNC_PATH/apt-packages/$(hostname)_current.txt
 }
+
+function restore_apt_packages() {
+    local 'file'
+    file=$(last_backup_version apt-packages txt)
+
+    sudo apt update
+
+    echo "restoring apt packages..."
+    /bin/cat $file | /bin/grep -v deinstall | /bin/awk -F' ' '{print $1}' ORS=' ' | xargs sudo apt install --yes
+
+    echo "removing unused apt packages..."
+    /bin/cat $file | /bin/grep deinstall | /bin/awk -F' ' '{print $1}' ORS=' ' | xargs sudo apt purge --yes
+}
