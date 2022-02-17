e656dfb8a5ce10eb083a46fe8be7c7b87232ed5c
Bernardo Loureiro <bernardo.lou@gmail.com>
2022-02-17 16:35:20 -0300

    Updating go version

--- 3rd-party-apps/go.sh
+++ 3rd-party-apps/go.sh
@@ -5,7 +5,7 @@ source $BASE_PATH/msg.sh
 msg_init 'go'
 
 if [ ! -f /usr/local/go/bin/go ]; then
-    wget --quiet https://golang.org/dl/go1.15.6.linux-amd64.tar.gz -O ~/tmp/go.tar.gz
+    wget --quiet https://golang.org/dl/go1.16.14.linux-amd64.tar.gz -O ~/tmp/go.tar.gz
     sudo tar -vC /usr/local -xzf ~/tmp/go.tar.gz
     export PATH=$PATH:/usr/local/go/bin:
 fi
