89a1be9bd13fc5560665551ded4ef5a4ad56120f
Bernardo Loureiro <bernardo.lou@gmail.com>
2022-04-27 12:06:16 -0300

    Fixing vpn add route

--- shell/add_vpn_custom_routes.sh
+++ shell/add_vpn_custom_routes.sh
@@ -19,6 +19,7 @@ function add_vpn_custom_routes() {
     function add_route() {
         local SOURCE_NET=$(format_network $1)
         local CMD="sudo ip route add $SOURCE_NET via $2"
+        echo "$CMD"
         eval $CMD 2>/dev/null
     }
 
@@ -28,50 +29,64 @@ function add_vpn_custom_routes() {
     local NOW=$(date +"%Y%m%d%H%M%S")
     local TUNNEL_ADDR=`tun0_ip`
 
-    init_file $NOW
+    if [[ "$TUNNEL_ADDR" == "" ]]; then
+        echo "VPN down"
+        return
+    fi
 
     echo "VPN address is ${TUNNEL_ADDR}"
 
-    while read line; do
-        if [[ `which_shell` == "zsh" ]]; then
-            parts=("${(@s/;/)line}")
-            host=${parts[1]}
-            up_at=${parts[2]}
-            down_at=${parts[3]}
-        else
-            IFS=';' read -r -a parts <<<"$line"
-            host=${parts[0]}
-            up_at=${parts[1]}
-            down_at=${parts[2]}
-        fi
+    if [[ "$1" == "" ]]; then
+        init_file $NOW
 
-        if [[ "$down_at" != "" ]]; then
-            echo $line | tee -a $SYNC_PATH/vpn-custom-routes/current.csv >/dev/null
-            continue
-        fi
+        while read line; do
+            if [[ `which_shell` == "zsh" ]]; then
+                parts=("${(@s/;/)line}")
+                host=${parts[1]}
+                up_at=${parts[2]}
+                down_at=${parts[3]}
+            else
+                IFS=';' read -r -a parts <<<"$line"
+                host=${parts[0]}
+                up_at=${parts[1]}
+                down_at=${parts[2]}
+            fi
 
-        IP_ADDRS=$(get_addrs $host)
-
-        if [[ "$IP_ADDRS" == "" ]]; then
-            echo "🔴 addr $host is down, exiting..."
-            echo $host";"$up_at";"$MOMENT | tee -a $SYNC_PATH/vpn-custom-routes/current.csv >/dev/null
-            continue
-        fi
-
-        echo $host";"$MOMENT";" | tee -a $SYNC_PATH/vpn-custom-routes/current.csv >/dev/null
-
-        IP_ADDRS_ARR=($(echo $IP_ADDRS | tr " " "\n"))
-
-        for j in "${IP_ADDRS_ARR[@]}"; do
-            echo "🔵 routing $j from $host to $TUNNEL_ADDR"
-            add_route $j $TUNNEL_ADDR
-            if [[ "$?" == "1" ]]; then
-                echo "🔴 fail to route"
+            if [[ "$down_at" != "" ]]; then
+                echo $line | tee -a $SYNC_PATH/vpn-custom-routes/current.csv >/dev/null
                 continue
             fi
-            echo "🟢 route complete"
-        done
 
-        IP_ADDRS=/dev/null
-    done < $SYNC_PATH/vpn-custom-routes/$NOW.csv
+            IP_ADDRS=$(get_addrs $host)
+
+            if [[ "$IP_ADDRS" == "" ]]; then
+                echo "🔴 addr $host is down, exiting..."
+                echo $host";"$up_at";"$MOMENT | tee -a $SYNC_PATH/vpn-custom-routes/current.csv >/dev/null
+                continue
+            fi
+
+            echo $host";"$MOMENT";" | tee -a $SYNC_PATH/vpn-custom-routes/current.csv >/dev/null
+
+            IP_ADDRS_ARR=($(echo $IP_ADDRS | tr " " "\n"))
+
+            for j in "${IP_ADDRS_ARR[@]}"; do
+                echo "🔵 routing $j from $host to $TUNNEL_ADDR"
+                add_route $j $TUNNEL_ADDR
+                if [[ "$?" == "1" ]]; then
+                    echo "🔴 fail to route"
+                    continue
+                fi
+                echo "🟢 route complete"
+            done
+
+            IP_ADDRS=/dev/null
+        done < $SYNC_PATH/vpn-custom-routes/$NOW.csv
+    else
+        IP_ADDRS=$(get_addrs $1)
+        IP_ADDRS_ARR=($(echo $IP_ADDRS | tr " " "\n"))
+        for j in "${IP_ADDRS_ARR[@]}"; do
+            echo "adding $j to VPN"
+            add_route $j $TUNNEL_ADDR
+        done
+    fi
 }
