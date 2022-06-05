608289a8b54edc58eee5b8b4a71404a7cc184f0e
Bernardo Loureiro <bernardo.lou@gmail.com>
2022-06-04 22:18:29 -0300

    Disabling some installs

--- 3rd-party-apps/guake.sh
+++ 3rd-party-apps/guake.sh
@@ -4,12 +4,14 @@ source $BASE_PATH/msg.sh
 
 msg_init 'guake'
 
-if [[ "$(command -v guake)" == "" ]]; then
-    git clone https://github.com/Guake/guake.git ~/.guake
-    cd ~/.guake
-    ./scripts/bootstrap-dev-debian.sh run make
-    make
-    sudo make install
-fi
+# NOTE: Installing by apt
+
+# if [[ "$(command -v guake)" == "" ]]; then
+#     git clone https://github.com/Guake/guake.git ~/.guake
+#     cd ~/.guake
+#     ./scripts/bootstrap-dev-debian.sh run make
+#     make
+#     sudo make install
+# fi
 
 msg_end 'guake'
--- 3rd-party-apps/tixati.sh
+++ 3rd-party-apps/tixati.sh
@@ -4,8 +4,8 @@ source $BASE_PATH/msg.sh
 
 msg_init 'tixati'
 
-if [[ "$(command -v tixati)" == "" ]]; then
-    curl https://download2.tixati.com/download/tixati_2.75-1_amd64.deb -o ~/tmp/tixati.deb && sudo dpkg -i ~/tmp/tixati.deb
-fi
+# if [[ "$(command -v tixati)" == "" ]]; then
+#     curl https://download2.tixati.com/download/tixati_2.75-1_amd64.deb -o ~/tmp/tixati.deb && sudo dpkg -i ~/tmp/tixati.deb
+# fi
 
 msg_end 'tixati'
