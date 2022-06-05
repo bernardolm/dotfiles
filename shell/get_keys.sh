1880394212aeda4e9e891851bb82a733500edfcb
Bernardo Loureiro <bernardo.lou@gmail.com>
2022-06-04 22:21:05 -0300

    Forking keys import

--- shell/get_keys.sh
+++ shell/get_keys.sh
@@ -12,7 +12,7 @@ function get_keys() {
     sudo apt-get update 2>&1 1>/dev/null | sed -ne 's/.*NO_PUBKEY //p' |
     while read key; do
         echo "processing key: $key"
-        sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys "$key"
+        sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys "$key" &
     done
 
     # Others
