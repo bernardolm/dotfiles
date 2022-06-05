38c02b4da16ce37cea640cc4bddcd3c4d90016a1
Bernardo Loureiro <bernardo.lou@gmail.com>
2022-06-04 22:19:40 -0300

    Adding docker desktop

--- 3rd-party-apps/docker.sh
+++ 3rd-party-apps/docker.sh
@@ -5,7 +5,7 @@ source $BASE_PATH/msg.sh
 msg_init 'docker'
 
 if [[ "$(command -v docker)" == "" ]]; then
-    sudo apt-get install --yes docker-ce docker-ce-cli containerd.io
+    sudo apt-get install --yes docker-ce docker-desktop docker-ce-cli containerd.io
     [ `/bin/cat /etc/group | grep docker | wc -l | bc` -eq 0 ] && sudo groupadd docker && newgrp docker
     [ `getent group docker | grep $USER | wc -l | bc` -eq 0 ] && sudo usermod -aG docker $USER
 fi
