4c0f10619abf08176ffc5c52df1f2da4c6ca8dc1
Bernardo Loureiro <bernardo.lou@gmail.com>
2022-06-06 19:18:36 -0300

    Adding gnome smart backup and restore

--- shell/gnome.sh
+++ shell/gnome.sh
@@ -1,13 +1,10 @@
 function backup_gnome() {
-    dconf dump /org/gnome/ > "$SYNC_PATH/backup_of_my_gnome_settings"
+    local now=$(date +"%Y%m%d%H%M%S")
+    dconf dump /org/gnome/ > "$SYNC_PATH/gnome/$now.txt"
 }
 
 function restore_gnome() {
+    local file=$(last_backup_version gnome txt)
     dconf reset -f /org/gnome/
-    if [[ `hostname` == hunb* ]]; then
-        dconf load /org/gnome/ <$SYNC_PATH/backup_of_my_gnome_settings
-    else
-        /bin/cat "$SYNC_PATH/backup_of_my_gnome_settings" | grep -v "text-scaling-factor" >~/tmp/backup_of_my_gnome_settings
-        dconf load /org/gnome/ <~/tmp/backup_of_my_gnome_settings
-    fi
+    dconf load /org/gnome/ <$file
 }
