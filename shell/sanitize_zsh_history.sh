7fb5ed0db93d15dbe7957b5525999080abfd94f4
Bernardo Loureiro <bernardo.lou@gmail.com>
2022-06-16 01:05:02 -0300

    Improving zsh tools

--- shell/sanitize_zsh_history.sh
+++ shell/sanitize_zsh_history.sh
@@ -1,10 +1,14 @@
 function sanitize_zsh_history() {
-    echo "history file is $HISTFILE"
-    local histfile_lines=`/bin/cat $HISTFILE | wc -l`
-    $DEBUG_SHELL && echo "$histfile_lines lines in $HISTFILE at begin"
+    backup_it $HISTFILE
+    local new_histfile="${HISTFILE}_NEW_$(date +"%Y%m%d%H%M%S%N")"
+    cp $HISTFILE $new_histfile
+    echo "history file is $new_histfile"
+    local histfile_lines=`/bin/cat --squeeze-blank $new_histfile | wc -l`
+    $DEBUG_SHELL && echo "$histfile_lines lines in $new_histfile at begin"
 
     local function list_files() {
-        find -L ~ -mindepth 1 -maxdepth 1 -regex '.*\.zsh_history[ \._]+.*'
+        find -L $HOME -size -100M -regex '.*\.zsh_history[ \._]+.*' 2>/dev/null \
+            | /bin/grep -v "$new_histfile"
     }
 
     local function count_current() {    
@@ -18,6 +22,8 @@ function sanitize_zsh_history() {
     local total=$last
     local x=0
 
+    [ $total -eq 0 ] && msg_end "no files found and" && return
+
     msg_start "merging of $total files"
 
     echo $files | while read filename; do
@@ -29,34 +35,37 @@ function sanitize_zsh_history() {
 
         $DEBUG_SHELL && echo "🔹 $filename "
 
-        local filename_lines=`/bin/cat $filename | wc -l`
+        local filename_lines=`/bin/cat --squeeze-blank $filename | wc -l`
         $DEBUG_SHELL && echo "$filename_lines lines in $filename"
         if [ $filename_lines -gt 0 ]; then
-            /bin/sed -i '/^\:/d' "$filename"
+            local cleaned="${filename}_clean"
+            touch $cleaned
+            /bin/sed '/\x0/d' "$filename" > $cleaned
+            mv -f $cleaned "$filename"
             
-            local filename_lines=`/bin/cat $filename | wc -l`
+            local filename_lines=`/bin/cat --squeeze-blank $filename | wc -l`
             $DEBUG_SHELL && echo "$filename_lines lines in $filename after cleaning"
             
             [ $filename_lines -le 0 ] && continue
 
-            local histfile_lines=`/bin/cat $HISTFILE | wc -l`
-            $DEBUG_SHELL && echo "$histfile_lines lines in $HISTFILE"
+            local histfile_lines=`/bin/cat --squeeze-blank $new_histfile | wc -l`
+            $DEBUG_SHELL && echo "$histfile_lines lines in $new_histfile"
 
-            /bin/cat $filename | tee -a $HISTFILE 1>/dev/null
+            /bin/cat --squeeze-blank $filename | tee -a $new_histfile 1>/dev/null
             
-            local histfile_lines=`/bin/cat $HISTFILE | wc -l`
-            $DEBUG_SHELL && echo "$histfile_lines lines in $HISTFILE at final"
+            local histfile_lines=`/bin/cat --squeeze-blank $new_histfile | wc -l`
+            $DEBUG_SHELL && echo "$histfile_lines lines in $new_histfile at final"
         fi
         /bin/rm "$filename"
 
         [ $x -lt 25 ] && let "x=x+1" && continue
         let "x=0"
 
-        $DEBUG_SHELL && echo "increase history file to "`get_file_size $HISTFILE`" size"
+        $DEBUG_SHELL && echo "increase history file to "`get_file_size $new_histfile`" size"
 
-        [ `stat -c '%s' $HISTFILE` -ge 10000000 ] && zsh-history-clear --file $HISTFILE
+        [ `stat -c '%s' $new_histfile` -ge 1000 ] && zsh-history-clear --file $new_histfile || true
     done
-    zsh-history-clear --file $HISTFILE
-    # backup_it $HISTFILE
-    msg_end "🏁 finish file with size "`get_file_size $HISTFILE`
+    zsh-history-clear --file $new_histfile
+    backup_it $new_histfile
+    msg_end "🏁 finish file with size "`get_file_size $new_histfile`
 }
