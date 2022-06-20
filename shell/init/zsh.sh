3f4d5b7f6361bb5fb05ccaf12b26943e4aecea5c
Bernardo Loureiro <bernardo.lou@gmail.com>
2022-06-20 19:38:51 -0300

    Adding zsh options

--- shell/init/zsh.sh
+++ shell/init/zsh.sh
@@ -1,12 +1,25 @@
+setopt autocd
+setopt beep
 setopt extended_history
+setopt extendedglob
 setopt hist_ignore_dups
 setopt inc_append_history
+setopt nomatch
+setopt notify
 setopt share_history
 unsetopt hist_save_by_copy
 
 [ ! $DEBUG_SHELL ] && setopt promptsubst
 
+export SAVEHIST=999999
+
 export HISTDUP=erase # Erase duplicates in the history file
 export HISTFILE=$SYNC_PATH/.zsh_history # Where to save history to disk
-export HISTSIZE=999999
-export SAVEHIST=$HISTSIZE
+export HISTSIZE=$SAVEHIST
+
+bindkey -e
+
+zstyle :compinstall filename '/home/ubuntu/.zshrc'
+
+autoload -Uz compinit
+compinit
