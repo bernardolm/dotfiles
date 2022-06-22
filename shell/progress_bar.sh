937dc5dd24a50e45844747309f5f5c8ef8da22ea
Bernardo Loureiro <bernardo.lou@gmail.com>
2022-06-22 18:01:45 -0300

    Adding message to progress bar

--- shell/progress_bar.sh
+++ shell/progress_bar.sh
@@ -2,30 +2,32 @@ function progress_bar() {
     local bar_size=300
     local bar=""
     local current=$1
-    local left_char="${GREEN}█${NC}"
-    local right_char="${RED}░${NC}"
+    local left_char="${GREEN}|${NC}" # █
+    local right_char="${RED}|${NC}" # ░
     local target=$2
+    local msg=$([ "$3" != "" ] && echo "$3 | " || echo "")
 
     local terminal_size=$(tput cols)
     local target_char_size=$(expr length $target)
-    local left_message="▁ $(eval printf %${target_char_size}s $current)/$target "
-    
+    # local left_message="▁ ${SKYBLUE}$msg${NC} | $(eval printf %${target_char_size}s $current)/$target "
+    local left_message="▁ ${msg}$(eval printf %${target_char_size}s $current)/$target "
+
     local percent
-    ((percent=100*$current/$target))
+    ((percent = 100 * $current / $target))
     local right_message=" $(printf %3s $percent)%"
 
     local messages_size=$(expr length "${left_message}${right_message}")
     local total_size
-    ((total_size=$messages_size+$bar_size))
+    ((total_size = $messages_size + $bar_size))
 
     if [ $total_size -gt $terminal_size ]; then
-        ((bar_size=$terminal_size-$messages_size));
+        ((bar_size = $terminal_size - $messages_size))
     fi
 
     local position
-    ((position=($bar_size*$percent)/100))
+    ((position = ($bar_size * $percent) / 100))
 
-    for ((left=0; left<$position; left++)); do bar+=$left_char; done
-    for ((right=$position; right<$bar_size; right++)); do bar+=$right_char; done
+    for ((left = 0; left < $position; left++)); do bar+=$left_char; done
+    for ((right = $position; right < $bar_size; right++)); do bar+=$right_char; done
     echo -ne "$left_message$bar$right_message\r"
 }
