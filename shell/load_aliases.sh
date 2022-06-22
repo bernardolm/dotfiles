78eff9bbed75b86569dd6140348ec0786dd2072c
Bernardo Loureiro <bernardo.lou@gmail.com>
2022-06-22 18:25:05 -0300

    Refactoring aliases loader

--- shell/load_aliases.sh
+++ shell/load_aliases.sh
@@ -1,8 +1,10 @@
 function load_aliases() {
-    [ -f $SYNC_PATH/aliases ] \
-        && (($DEBUG_SHELL && echo "loading sync path aliases in $SYNC_PATH/aliases") || true) \
-        && source $SYNC_PATH/aliases
-    [ -f $DOTFILES/aliases ] \
-        && (($DEBUG_SHELL && echo "loading git path aliases in $DOTFILES/aliases") || true) \
-        && source $DOTFILES/aliases
+    local paths=("$SYNC_PATH/aliases" "$DOTFILES/aliases")
+    
+    for f in $paths; do
+        if [ -f $f ]; then
+            $DEBUG_SHELL && echo "loading path aliases in $f"
+            source $f
+        fi
+    done
 }
