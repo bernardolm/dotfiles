2dea25289de56abe9ba38d7f5320c2175b9724f4
Bernardo Loureiro <bernardo.lou@gmail.com>
2022-06-22 17:56:26 -0300

    Changing some spaceship config

--- shell/init/spaceship.sh
+++ shell/init/spaceship.sh
@@ -7,7 +7,7 @@ function spaceship_histsize() {
     # http://zsh.sourceforge.net/Doc/Release/Shell-Grammar.html#Aliasing
     local 'histfile_size'
 
-    histfile_size=`[ -f $HISTFILE ] && echo -n $(wc -l < $HISTFILE)`
+    histfile_size=$([ -f $HISTFILE ] && echo -n $(wc -l <$HISTFILE))
 
     # Exit section if variable is empty
     [[ -z $histfile_size ]] && return
@@ -24,27 +24,27 @@ function spaceship_histsize() {
 }
 
 export SPACESHIP_PROMPT_ORDER=(
-    time              # Time stamps section
-    histsize
-    dir               # Current directory section
-    host              # Hostname section
-    git               # Git section (git_branch + git_status)
-    package           # Package version
-    node              # Node.js section
-    ruby              # Ruby section
-    golang            # Go section
-    php               # PHP section
-    docker_context    # Docker context
-    aws               # Amazon Web Services section
-    gcloud            # Google Cloud Platform section
-    venv              # virtualenv section
-    kubectl           # Kubectl context section
-    battery           # Battery level and status
-    jobs              # Background jobs indicator
-    random_emoji      # Random emoji to better distinct terminals
-    exit_code         # Exit code section
-    exec_time         # Execution time
-    char              # Prompt character
+    time           # Time stamps section
+    histsize       # ZSH command history counter
+    dir            # Current directory section
+    host           # Hostname section
+    git            # Git section (git_branch + git_status)
+    package        # Package version
+    node           # Node.js section
+    ruby           # Ruby section
+    golang         # Go section
+    php            # PHP section
+    docker_context # Docker context
+    aws            # Amazon Web Services section
+    gcloud         # Google Cloud Platform section
+    venv           # virtualenv section
+    kubectl        # Kubectl context section
+    battery        # Battery level and status
+    jobs           # Background jobs indicator
+    exit_code      # Exit code section
+    exec_time      # Execution time
+    random_emoji   # Random emoji to better distinct terminals
+    char           # Prompt character
 )
 
 export LS_COLORS=$LS_COLORS:'ow=01;34:'
@@ -53,16 +53,16 @@ export SPACESHIP_BATTERY_SYMBOL_FULL=""
 export SPACESHIP_BATTERY_THRESHOLD=25
 export SPACESHIP_CHAR_SYMBOL_ROOT="🚨 "
 export SPACESHIP_DIR_PREFIX='📂 in '
-export SPACESHIP_DIR_TRUNC=2
+# export SPACESHIP_DIR_TRUNC=3
 export SPACESHIP_DOCKER_SHOW=true
 export SPACESHIP_EXEC_TIME_ELAPSED=0.1
-export SPACESHIP_EXEC_TIME_PREFIX="⏱ "
+export SPACESHIP_EXEC_TIME_PREFIX="⏳ "
 export SPACESHIP_EXIT_CODE_COLOR="white"
 export SPACESHIP_EXIT_CODE_SHOW=true
 export SPACESHIP_EXIT_CODE_SYMBOL="🚧 "
 export SPACESHIP_GCLOUD_SHOW=false
 export SPACESHIP_GIT_BRANCH_SUFFIX=""
-export SPACESHIP_GIT_PREFIX='git:('$SPACESHIP_CHAR_SYMBOL
+export SPACESHIP_GIT_PREFIX='📇 git:('$SPACESHIP_CHAR_SYMBOL
 export SPACESHIP_GIT_SUFFIX=") "
 export SPACESHIP_HISTSIZE_SHOW=true
 export SPACESHIP_JOBS_SHOW=true
