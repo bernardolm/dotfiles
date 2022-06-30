3ceeeeb28af0e46a009729e10244ee569587ed96
Bernardo Loureiro <bernardo.lou@gmail.com>
2022-06-30 13:41:53 -0300

    Skipping to load scripts with hash bang

--- shell/init/functions.sh
+++ shell/init/functions.sh
@@ -1,11 +1,17 @@
 local functions_paths=("$DOTFILES/shell" "$SYNC_PATH/shell")
 
 for fp in $functions_paths; do
-    $DEBUG_SHELL && echo "\t🔎 searching function script files in $fp"
+    $DEBUG_SHELL && echo "\t\t🔎 searching function script files in $fp"
 
     for NAME in $(find $fp/*.sh); do
         [[ "$(basename $NAME)" == "functions.sh" ]] && continue
-        $DEBUG_SHELL && echo "\t\t📄 loading function script `basename $NAME`"
+
+        if [ `/bin/cat $NAME | head -1 | grep -c '#!'` -gt 0 ]; then
+            $DEBUG_SHELL && echo "\t\t\t⏭  hashbang found on $(basename $NAME), skipping..."
+            continue
+        fi
+
+        $DEBUG_SHELL && echo "\t\t\t📄 loading function script `basename $NAME`"
         source $NAME
     done
 done
--- shell/init/init.sh
+++ shell/init/init.sh
@@ -10,11 +10,19 @@ local function load_custom_script() {
     local script=$1
     local tabs=$2
     local script_file=`basename $script`
-    $DEBUG_SHELL && [[ "$script_file" != "functions_loader.sh" ]] \
-        && echo -n "${tabs}📄 checking $script_file..." 
+
+    $DEBUG_SHELL && echo -n "${tabs}📄 checking $script_file..."
+
+    [ $script_file = "functions.sh" ] && echo ""
+
+    if [ `/bin/cat $script | head -1 | grep -c '#!'` -gt 0 ]; then
+        $DEBUG_SHELL && echo " hashbang found on $script_file, skipping..."
+        return
+    fi
+
     if [[ ! "($INIT_SCRIPTS)" =~ "$script_file" ]]; then
         source $script
-        $DEBUG_SHELL && [[ "$script_file" != "functions_loader.sh" ]] \
+        $DEBUG_SHELL && [[ "$script_file" != "functions.sh" ]] \
             && echo " loaded."
     else
         $DEBUG_SHELL && echo " already loaded."
@@ -23,10 +31,12 @@ local function load_custom_script() {
 
 for scripts_path in $init_scripts_path; do
     $DEBUG_SHELL && echo "📁 loading path $scripts_path"
+
     if [ ! -d "$scripts_path" ]; then
         load_custom_script $scripts_path "\t"
     else
         $DEBUG_SHELL && echo "\t🔎 searching scripts in path `basename $scripts_path`"
+
         for another_scripts_path in $(find $scripts_path/*.sh); do
             load_custom_script $another_scripts_path "\t\t"
         done
