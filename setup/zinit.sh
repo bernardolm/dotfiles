a6d3bb49ee8f6b5c5976d4839fbbe0bb2737a162
Bernardo Loureiro <bernardo.lou@gmail.com>
2022-07-08 16:35:48 -0300

    Adding zsh extentions to test

--- setup/zinit.sh
+++ setup/zinit.sh
@@ -49,16 +49,77 @@ zinit_plugins=(
     zdharma-continuum/zinit-annex-rust
 )
 
-zinit_plugins_turbo=(
+xzinit_plugins_turbo=(
+    # djui/alias-tips
+    # rahulsalvi/velocity-python
+    # unixorn/1password-op.plugin.zsh
+    3v1n0/zsh-bash-completions-fallback
+    ael-code/zsh-colored-man-pages
+    b4b4r07/emoji-cli
+    bric3/nice-exit-code
+    brymck/print-alias
+    chrissicool/zsh-256color
+    commiyou/zsh-editing-workbench
+    eastokes/aws-plugin-zsh
+    folixg/gimme-ohmyzsh-plugin
+    gmatheu/shell-plugins
+    gretzky/auto-color-ls
+    gryffyn/mouse-status
+    jedahan/geometry-hydrate
+    jgogstad/passwordless-history
+    jimhester/per-directory-history
     johanhaleby/kubetail
+    joshskidmore/zsh-fzf-history-search
+    kazhala/dotbare
+    laggardkernel/zsh-thefuck
     mafredri/zsh-async
+    marlonrichert/zcolors
+    marlonrichert/zsh-autocomplete
+    mattmc3/zsh-safe-rm
+    MenkeTechnologies/zsh-cargo-completion
+    MichaelAquilina/zsh-auto-notify
+    MichaelAquilina/zsh-you-should-use
+    MisterRios/stashy
+    molovo/revolver
     ohmyzsh/ohmyzsh
+    paulmelnikow/zsh-startup-timer
+    pschmitt/emoji-fzf.zsh
+    QuarticCat/zsh-smartcache
+    rawkode/zsh-docker-run
+    reegnz/jq-zsh-plugin
+    RobSis/zsh-completion-generator
+    romkatv/zsh-bench
+    se-jaeger/zsh-activate-py-environment
+    sei40kr/fast-alias-tips-bin
+    sei40kr/zsh-fast-alias-tips
+    srijanshetty/docker-zsh
+    srijanshetty/zsh-pip-completion
+    sroze/docker-compose-zsh-plugin
+    sunlei/zsh-ssh
+    TamCore/autoupdate-oh-my-zsh-plugins
+    Tarrasch/zsh-command-not-found
+    unixorn/fzf-zsh-plugin
+    Valiev/almostontop
+    vercel/zsh-theme
+    vladmyr/dotfiles-plugin
+    willghatch/zsh-saneopt
+    wuotr/zsh-plugin-vscode
+    xylous/alias-zsh
+    z-shell/zsh-diff-so-fancy
     zdharma-continuum/fast-syntax-highlighting
     zdharma-continuum/history-search-multi-word
+    zdharma-continuum/zbrowse
     zdharma-continuum/zsh-navigation-tools
     zdharma/fast-syntax-highlighting
+    zpm-zsh/colorize
+    zpm-zsh/dropbox
+    zpm-zsh/figures
+    zpm-zsh/pr-return
     zsh-users/zsh-autosuggestions
     zsh-users/zsh-completions
+    zsh-users/zsh-history-substring-search
+    zsh-users/zsh-syntax-highlighting
+    # zshzoo/cd-ls
 )
 
 $DEBUG_SHELL && typeset -g ZPLG_MOD_DEBUG=1
@@ -78,6 +139,8 @@ if [[ ! -d $ZINIT_ROOT ]]; then
 
     zinit snippet OMZL::git.zsh
     zinit snippet OMZP::git
+
+    zinit ice from'gh-r' as'program'
 
     zinit_runner "echo" $zinit_plugins_off
     zinit_runner "debug" $zinit_plugins
