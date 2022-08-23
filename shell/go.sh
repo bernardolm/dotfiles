4ae9b64430d04d47fa1729f20e7656254ba57f89
Bernardo Loureiro <bernardo.lou@gmail.com>
2022-08-23 15:45:40 -0300

    Fixing go tool install commands

--- shell/go.sh
+++ shell/go.sh
@@ -12,37 +12,37 @@ function go_install_tools() {
 
     curl -sSfL https://raw.githubusercontent.com/golangci/golangci-lint/master/install.sh | sh -s -- -b $(go env GOPATH)/bin
 
-    go get -v github.com/acroca/go-symbols
-    go get -v github.com/cortesi/modd/cmd/modd
-    go get -v github.com/cweill/gotests/...
-    go get -v github.com/davidrjenni/reftools/cmd/fillstruct
-    go get -v github.com/fatih/gomodifytags
-    go get -v github.com/go-delve/delve/cmd/dlv
-    go get -v github.com/godoctor/godoctor
-    go get -v github.com/golangci/golangci-lint/cmd/golangci-lint
-    go get -v github.com/haya14busa/goplay/cmd/goplay
-    go get -v github.com/josharian/impl
-    go get -v github.com/liudng/dogo
-    go get -v github.com/mdempsky/gocode
-    go get -v github.com/mgechev/revive
-    go get -v github.com/odeke-em/drive/cmd/drive
-    go get -v github.com/pkg/profile
-    go get -v github.com/ramya-rao-a/go-outline
-    go get -v github.com/rogpeppe/godef
-    go get -v github.com/sqs/goreturns
-    go get -v github.com/stamblerre/gocode
-    go get -v github.com/tsenart/vegeta
-    go get -v github.com/tylerb/gotype-live
-    go get -v github.com/uudashr/gopkgs/v2/cmd/gopkgs
-    go get -v github.com/zmb3/gogetdoc
-    go get -v golang.org/x/lint/golint
-    go get -v golang.org/x/tools/cmd/goimports
-    go get -v golang.org/x/tools/cmd/gorename
-    go get -v golang.org/x/tools/cmd/guru
-    go get -v golang.org/x/tools/gopls
-    go get -v honnef.co/go/tools/...
-    go get -v mvdan.cc/sh/v3/cmd/shfmt
-    go get -v winterdrache.de/goformat/goformat
+    go install -v github.com/acroca/go-symbols@latest
+    go install -v github.com/cortesi/modd/cmd/modd@latest
+    go install -v github.com/cweill/gotests/...@latest
+    go install -v github.com/davidrjenni/reftools/cmd/fillstruct@latest
+    go install -v github.com/fatih/gomodifytags@latest
+    go install -v github.com/go-delve/delve/cmd/dlv@latest
+    go install -v github.com/godoctor/godoctor@latest
+    go install -v github.com/golangci/golangci-lint/cmd/golangci-lint@latest
+    go install -v github.com/haya14busa/goplay/cmd/goplay@latest
+    go install -v github.com/josharian/impl@latest
+    go install -v github.com/liudng/dogo@latest
+    go install -v github.com/mdempsky/gocode@latest
+    go install -v github.com/mgechev/revive@latest
+    go install -v github.com/odeke-em/drive/cmd/drive@latest
+    go install -v github.com/pkg/profile@latest
+    go install -v github.com/ramya-rao-a/go-outline@latest
+    go install -v github.com/rogpeppe/godef@latest
+    go install -v github.com/sqs/goreturns@latest
+    go install -v github.com/stamblerre/gocode@latest
+    go install -v github.com/tsenart/veinstalla@latest
+    go install -v github.com/tylerb/gotype-live@latest
+    go install -v github.com/uudashr/gopkgs/v2/cmd/gopkgs@latest
+    go install -v github.com/zmb3/goinstalldoc@latest
+    go install -v golang.org/x/lint/golint@latest
+    go install -v golang.org/x/tools/cmd/goimports@latest
+    go install -v golang.org/x/tools/cmd/gorename@latest
+    go install -v golang.org/x/tools/cmd/guru@latest
+    go install -v golang.org/x/tools/gopls@latest
+    go install -v honnef.co/go/tools/...@latest
+    go install -v mvdan.cc/sh/v3/cmd/shfmt@latest
+    go install -v winterdrache.de/goformat/goformat@latest
 
     echo "install finish"
 }
