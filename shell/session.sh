38d09f6952091f03c578ecde24a57e101800e4fa
Bernardo Loureiro <bernardo.lou@gmail.com>
2022-09-09 09:26:36 -0300

    Refactoring scripts loader

--- shell/session.sh
+++ shell/session.sh
@@ -1,55 +1,75 @@
-function start_zsh_session() {
-    zsh_start_session=/tmp/zsh_session
-    [ ! -d $zsh_start_session ] && mkdir -p $zsh_start_session
+#!/bin/bash
 
-    export ZSH_TOTAL=$(ls -Art $zsh_start_session | tail -n1 | xargs -I{} /bin/cat -b {} | wc -l)
-    $DEBUG_SHELL && echo "loading $ZSH_TOTAL scripts"
-
-    zsh_start_session+='/'`date +"%Y%m%d%H%M%S%N"`
-    touch $zsh_start_session
+start_zsh_session() {
+    local zsh_start_session=/tmp/zsh_session
+    ZSH_TOTAL=100
+    if [ ! -d $zsh_start_session ]; then
+        mkdir -p $zsh_start_session
+    else
+        local file
+        file=$(find "$zsh_start_session" -type f | tail -1)
+        if [ -n "$file" ]; then
+            ZSH_TOTAL=$(/bin/cat -b "$file" | wc -l)
+        fi
+        $DEBUG_SHELL && printf "loading %d scripts...\n" "$ZSH_TOTAL"
+    fi
+    export ZSH_TOTAL
+    zsh_start_session+='/'$(date +"%Y%m%d%H%M%S%N")
+    touch "$zsh_start_session"
     export ZSH_SESSION=$zsh_start_session
     alias source="source_and_log_session"
 }
 
-function log_zsh_session() {
-    file=`realpath $1`
+log_zsh_session() {
+    local file
+    file=$(realpath "$1")
     [ -e "$ZSH_SESSION" ] \
-        && { echo $file | tee -a $ZSH_SESSION 1>/dev/null; }
+        && { echo "$file" | tee -a "$ZSH_SESSION" 1>/dev/null; }
 }
 
-function check_zsh_session() {
-    file=`realpath $1`
+check_zsh_session() {
+    local file
+    file=$(realpath "$1")
     [ -e "$ZSH_SESSION" ] \
-        && grep $file "$ZSH_SESSION" && return
+        && grep "$file" "$ZSH_SESSION" && return
     false
 }
 
-function finish_zsh_session() {
-    command -v progress_bar &>/dev/null || source $DOTFILES/shell/progress_bar.sh
+finish_zsh_session() {
+    # shellcheck source=/dev/null
+    command -v progress_bar &>/dev/null || . "$DOTFILES/shell/progress_bar.sh"
 
-    local position=$(/bin/cat -b $ZSH_SESSION | wc -l)
-    if [ $position -le $ZSH_TOTAL ]; then
-        progress_bar $position $ZSH_TOTAL
-    fi
-    if [ $position -eq $ZSH_TOTAL ]; then
-        echo "$position lines in deleted session"
+    if [ -n "$ZSH_SESSION" ]; then
+        local position
+        position=$(/bin/cat -b "$ZSH_SESSION" | wc -l)
+        if [ -n "$position" ]; then
+            if [ "$position" -le "$ZSH_TOTAL" ]; then
+                progress_bar "$position" "$ZSH_TOTAL"
+            fi
+            if [ "$position" -eq "$ZSH_TOTAL" ]; then
+                echo "$position lines in deleted session"
+            fi
+        fi
     fi
 }
 
-function source_and_log_session() {
-    file=`realpath $1`
+source_and_log_session() {
+    local file checked
+    file=$(realpath "$1")
+    checked=$(check_zsh_session "$file")
 
     $DEBUG_SHELL && echo -n "${ORANGE}checking if ${TAN}$file ${ORANGE}was loaded... "
 
-    if [ `check_zsh_session $file` ]; then
+    if [ "$checked" ]; then
         $DEBUG_SHELL && echo "${RED}yes!${NC}"
         return
     fi
 
     $DEBUG_SHELL && echo "${GREEN}nope${NC}"
 
-    log_zsh_session $file
-    . $file
+    log_zsh_session "$file"
+    # shellcheck source=/dev/null
+    . "$file"
     finish_zsh_session
 
     $DEBUG_SHELL && echo "${TAN}$file ${SKYBLUE}load finish${NC}"
