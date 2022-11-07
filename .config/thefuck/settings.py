77ad1533c318009e7f5f1661515761dac9822f1c
Bernardo Loureiro <bernardo.lou@gmail.com>
2022-11-07 13:11:47 -0300

    Adding new files

---
+++ .config/thefuck/settings.py
@@ -0,0 +1,11 @@
+debug = True
+exclude_rules = ['git_push']
+history_limit = 9999999
+no_colors = False
+num_close_matches = 5
+priority = {'sudo': 100, 'no_command': 9999}
+require_confirmation = True
+rules = ['sudo', 'no_command']
+slow_commands = ['react-native', 'gradle']
+wait_command = 10
+wait_slow_command = 20
---
+++ thefuck/install.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+sudo apt update
+sudo apt install python3-dev python3-pip python3-setuptools
+pip3 install thefuck --user
