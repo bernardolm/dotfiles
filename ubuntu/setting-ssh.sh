3635b2ee0d22c2f80eb79e9fd4fe7c3061963b35
Bernardo Loureiro <bernardo.lou@gmail.com>
2022-11-08 22:27:50 -0300

    Removing trash

--- ubuntu/setting-ssh.sh
+++ ubuntu/setting-ssh.sh
@@ -1,15 +1,15 @@
 #!/usr/bin/env bash
 
-source ../init/env.sh
-source ../init/functions_loader.sh
+# source ../init/env.sh
+# source ../init/functions_loader.sh
 
 msg_start 'setting ssh'
 
 if [ ! -f ~/.ssh/id_ed25519 ]; then
-    ssh-keygen -t ed25519 -C `git config user.email`
+    ssh-keygen -t ed25519 -C "$(git config user.email)"
     eval "$(ssh-agent -s)"
     ssh-add ~/.ssh/id_ed25519
-    [ `command -v xclip` ] || sudo apt-get install xclip
+    [ "$(command -v xclip)" ] || sudo apt-get install xclip
     xclip -selection clipboard < ~/.ssh/id_ed25519.pub
     echo "The generated SSH key is on your clipboard. Now, you need to follow the steps in https://docs.github.com/pt/github/authenticating-to-github/adding-a-new-ssh-key-to-your-github-account to link your SSH key to github."
     read -p "Press enter to continue..." y
