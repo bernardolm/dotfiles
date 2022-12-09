0c00c3b8b5e5cabdbead444906e3f5433dd1843b
Bernardo Loureiro <bernardo.lou@gmail.com>
2022-12-09 15:33:35 -0300

    Updating guake cmds

--- zsh/guake.zsh
+++ zsh/guake.zsh
@@ -1,17 +1,8 @@
-function guake_backup() {
-    dconf dump /apps/guake/ > "$SYNC_PATH/backup_of_my_guake_settings"
-}
-
-function guake_restore() {
-    dconf reset -f /apps/guake/
-    dconf load /apps/guake/ < "$SYNC_PATH/backup_of_my_guake_settings"
-}
-
 function guake_update() {
     git --git-dir $WORKSPACE_USER/guake/.git checkout .
     git --git-dir $WORKSPACE_USER/guake/.git checkout master
+    git --git-dir $WORKSPACE_USER/guake/.git pull origin master
     sudo make --directory $WORKSPACE_USER/guake uninstall
     make --directory $WORKSPACE_USER/guake
     sudo make --directory $WORKSPACE_USER/guake install
-    make --directory $WORKSPACE_USER/guake reinstall
 }
