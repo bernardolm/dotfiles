6d08ad178e532cc5842c0fb499b996118cada4d8
Bernardo Loureiro <bernardo.lou@gmail.com>
2023-04-18 18:56:44 -0300

    Installing gnome extentions

--- zsh/gnome.zsh
+++ zsh/gnome.zsh
@@ -32,12 +32,3 @@ function gnome_shell_extensions_reload() {
     active_extensions=$(gsettings get org.gnome.shell enabled-extensions)
     gsettings set org.gnome.shell enabled-extensions "$active_extensions"
 }
-
-function gnome_shell_extensions_purge_dock() {
-    gsettings set org.gnome.shell.extensions.dash-to-dock autohide false
-    gsettings set org.gnome.shell.extensions.dash-to-dock dock-fixed false
-    gsettings set org.gnome.shell.extensions.dash-to-dock intellihide false
-    gsettings set org.gnome.shell enable-hot-corners false
-    sudo apt remove gnome-shell-extension-ubuntu-dock
-    gnome-extensions disable ubuntu-dock@ubuntu.com
-}
