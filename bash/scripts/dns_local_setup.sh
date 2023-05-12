b682f6b19b4f43168e37346ee73e54a8336244c0
Bernardo Loureiro <bernardo.lou@gmail.com>
2023-05-11 21:34:11 -0300

    Refactoring local dns setup

--- bash/scripts/dns_local_setup.sh
+++ bash/scripts/dns_local_setup.sh
@@ -5,31 +5,31 @@
 # P.S.: pristine Ubuntu linux don't come with curl, but wget.
 #
 # Dev notes:
-# you can call `python3 -m http.server` from dns_local_setup.sh file path to serve ir by http on port 8000.
+# you can call `python3 -m http.server` from dns_local_setup.sh folder location to serve it by http on port 8000.
 # wget -q -O - http://192.168.1.2:8000/dns_local_setup.sh | bash
 
 function dns_local_setup() {
     local container_name="${CONTAINER_NAME:-ns1}"
-    local custom_fallback_dns="${CUSTOM_FALLBACK_DNS-:}"
+    local custom_fallback_dns="${CUSTOM_FALLBACK_DNS:-8.8.4.4}"
     local domain="${DOMAIN:-hud}"
     local image_name="ruudud/devdns"
     local naming="unsafe"
-    local path_to_clone="${WORKSPACE_USER:-"$HOME/.devdns"}"
+    local workdir="${WORKSPACE_USER:-"$HOME/.devdns"}"
     # local prohibited_dns="${PROHIBITED_DNS:-'1.1.1.1,1.1.0.0,8.8.8.8,8.8.4.4'}"
 
     local gateway_ip
     gateway_ip=$(ip route show | grep -i 'default via' | awk '{ printf $3 }' | head -1)
 
     local has_curl
-    has_curl=$(command -v curl &>/dev/null && echo "yes")
+    has_curl=$(command -v curl 1>/dev/null && printf "yes")
 
     local has_docker
-    has_docker=$(command -v docker &>/dev/null && echo "yes")
+    has_docker=$(command -v docker 1>/dev/null && printf "yes")
 
     function sudo_advice() {
         printf "> ⚠ Your super password (sudo) is required.\n"
         tput sc
-        sudo ls &>/dev/null
+        sudo ls 1>/dev/null
         tput rc; tput el
     }
 
@@ -37,78 +37,68 @@ function dns_local_setup() {
         docker ps -a -q --filter "ancestor=${image_name}:latest"
     }
 
-    function kill_53_port_user() {
-        printf "> Killing process using port 53.\n"
-        sudo lsof -t -i:53 | while read -r pid; do
-            sudo kill -9 "$pid"
-        done
-        sudo killall -HUP dnsmasq 2>/dev/null
-    }
-
     function remove_local_dns_instances() {
         printf "> Removing docker containers using %s image if exists.\n" "$image_name"
-        local_dns_instances_running | while read -r container_id; do
-            printf "%s" "$container_id"
-            docker stop "$container_id" 2>/dev/null
-            docker rm "$container_id" 2>/dev/null
+        while : ; do
+            container_id=$(docker ps -a -q --filter "ancestor=${image_name}:latest" | head -n1)
+            [ "$container_id" == "" ] && break
+            printf "  Container %s found..." "$container_id"
+            docker stop "$container_id" 1>/dev/null || return 1
+            docker rm "$container_id" 1>/dev/null || return 1
+            printf " removed.\n"
         done
-    }
-
-    function build_local_dns_image() {
-        printf "> Building %s docker image.\n" "$image_name"
-        docker build \
-            --force-rm \
-            -f "$path_to_clone/Dockerfile" \
-            -t "$image_name" \
-            "$path_to_clone"
     }
 
     function show_resolv_conf() {
-        cat < /etc/resolv.conf | grep -v -e '^#' | grep -v -e '^\s*$' | grep --color=never 'nameserver'
+        printf "\033[1;36m"
+        printf "%s" "$1" | grep --color=never -v -e '^#' | grep --color=never -v -e '^\s*$' | grep --color=never 'nameserver'
+        printf "\033[0m"
     }
 
-    function show_network_manager_conf() {
-        cat < /etc/NetworkManager/NetworkManager.conf | grep -v -e '^#' | grep -v -e '^\s*$' | grep --color=never 'dns=dnsmasq'
+    function fixing_network_manager_conf() {
+        cat < /etc/NetworkManager/NetworkManager.conf | grep --color=never -v -e '^#' | grep --color=never -v -e '^\s*$' | grep --color=never -E '^dns=dnsmasq'
+        sudo sed -i.bak 's/dns=dnsmasq/#dns=dnsmasq/g' /etc/NetworkManager/NetworkManager.conf
     }
 
     function add_fallback_dns() {
         printf "> Configuring DNS resolver fallback\n"
-        has_gateway_ip=$(cat < /etc/resolv.conf | grep -v -e '^#' | grep -c "$gateway_ip")
+        local resolv_conf
+        resolv_conf=$(cat < /etc/resolv.conf | grep --color=never -v 'devdns:')
 
-        printf "  Your current resolv.conf has\033[1;36m"
-        show_resolv_conf
-        printf "\033[0m"
+        printf "  Your current resolv.conf has:\n"
+        show_resolv_conf "$resolv_conf"
 
-        if [[ "$has_gateway_ip" == "0" ]]; then
-            to_add="nameserver $gateway_ip\t# current DNS from your router gateway"
-            printf "%s" "$to_add" | sudo tee -a /etc/resolv.conf &>/dev/null
-
-            printf "  Your changed resolv.conf is\033[1;36m"
-            show_resolv_conf
-            printf "\033[0m"
+        if [[ "$gateway_ip" != "" ]]; then
+            resolv_conf=$(echo -ne "$resolv_conf\nnameserver $gateway_ip\t# devdns: current DNS from your router gateway\n")
         fi
 
-        if [[ -n "$custom_fallback_dns" ]]; then
-            to_add="nameserver $custom_fallback_dns\t# your custom DNS"
-            printf "%s" "$to_add" | sudo tee -a /etc/resolv.conf &>/dev/null
-
-            printf "  Your changed resolv.conf is\033[1;36m"
-            show_resolv_conf
-            printf "\033[0m"
+        if [[ "$custom_fallback_dns" != "" ]]; then
+            resolv_conf=$(echo -ne "$resolv_conf\nnameserver $custom_fallback_dns\t# devdns: your custom fallback DNS\n")
         fi
+
+        if [[ "$gateway_ip$custom_fallback_dns" != "" ]]; then
+            printf "  Your changed resolv.conf has:\n"
+            show_resolv_conf "$resolv_conf"
+        fi
+
+        printf "%s" "$resolv_conf" | sudo tee /etc/resolv.conf 1>/dev/null
+    }
+
+    function test_host() {
+        printf "  %s: %s..." "$1" "$2"
+        if [ "$(nslookup $2 | grep -c NXDOMAIN)" != "0" ]; then
+            printf " FAILED.\n"
+            return 1
+        fi
+        printf " OK.\n"
     }
 
     function test_hosts() {
         printf "> Testing name resolver.\n"
 
-        printf "   Created docker container.\n"
-        dig "$container_name.$domain" | grep --color=never "$container_name.$domain"
-
-        printf "  Some internet name.\n"
-        dig 'www.google.com' | grep --color=never 'www.google.com'
-
-        printf "  Some local network name.\n"
-        dig 'spinnaker.hucloud.com.br' | grep --color=never 'spinnaker.hucloud.com.br'
+        test_host "The docker container created" "$container_name.$domain" || return 1
+        test_host "Some internet name" "www.google.com" || return 1
+        test_host "Some local network name" "spinnaker.hucloud.com.br" || return 1
     }
 
     function start_local_dns() {
@@ -116,7 +106,8 @@ function dns_local_setup() {
 
         mkdir -p "$HOME/.$container_name/dnsmasq-hosts.d"
 
-        docker run -d \
+        newgrp docker 1>/dev/null || true
+        $(which docker) run -d \
             --name="$container_name" \
             --restart=always \
             -e DNS_DOMAIN="$domain" \
@@ -126,25 +117,23 @@ function dns_local_setup() {
             -v /etc/resolv.conf:/mnt/resolv.conf \
             -v /var/run/docker.sock:/var/run/docker.sock:ro \
             "$image_name"
-
-        docker logs -n-1 "$container_name"
     }
 
     function install_docker () {
         printf "  Installing docker.\n"
-        printf "  Your can check https://docs.docker.com/engine/install/ubuntu/ for more details.\n"
-        printf "  And is important check https://docs.docker.com/engine/install/linux-postinstall/#manage-docker-as-a-non-root-user too.\n"
-        printf "  Now calling install script from official docker site.\n\n"
-        curl -fsSL "https://get.docker.com" -o /tmp/get-docker.sh || return 1
-        sudo sh /tmp/get-docker.sh &>/dev/null || return 1
+        printf "  You can check link https://docs.docker.com/engine/install/ubuntu/ for more details.\n"
+        printf "  Is important to check link https://docs.docker.com/engine/install/linux-postinstall/#manage-docker-as-a-non-root-user too.\n"
+        printf "  Now the official docker installation script will be called.\n"
+        curl -fsSL "https://get.docker.com" -o /tmp/get-docker.sh
+        sudo sh /tmp/get-docker.sh 1>/dev/null
     }
 
     function setup_docker() {
-        printf "> Executing post install docker.\n"
-        getent group docker &>/dev/null || sudo groupadd docker &>/dev/null || return 1
-        sudo usermod -aG docker "$USER" || return 1
-        newgrp docker || return 1
-        sudo apt-get install -y uidmap &>/dev/null && dockerd-rootless-setuptool.sh install &>/dev/null || return 1
+        printf "  Running docker post install.\n"
+        getent group docker 1>/dev/null || sudo groupadd docker 1>/dev/null
+        sudo usermod -aG docker "$USER"
+        newgrp docker
+        sudo apt-get install --yes uidmap &>/dev/null && dockerd-rootless-setuptool.sh install 1>/dev/null
     }
 
     function check_dependencies() {
@@ -154,44 +143,99 @@ function dns_local_setup() {
             printf "  Curl OK.\n"
         else
             printf "  Installing curl.\n"
-            sudo apt-get install curl --yes 1>/dev/null || return 1
+            sudo apt-get install curl --yes &>/dev/null
         fi
 
         if [ "$has_docker" == "yes" ]; then
             printf "  Docker installed.\n"
         else
-            install_docker || return 1
+            install_docker
         fi
 
-        if docker run hello-world &>/dev/null; then
+        if docker run --rm hello-world 1>/dev/null; then
             printf "  Permission to run docker OK.\n"
         else
-            setup_docker || return 1
+            setup_docker
         fi
 
-        [ "$has_docker" == "yes" ] && docker run hello-world &>/dev/null && printf "  Docker OK.\n"
+        [ "$has_docker" == "yes" ] && docker run --rm hello-world 1>/dev/null && printf "  Docker OK.\n"
 
         printf "  All dependencies OK.\n"
     }
 
     function ensure_workdir() {
-        if [ ! -d "$path_to_clone" ]; then
-            mkdir -p "$path_to_clone"
+        if [ ! -d "$workdir" ]; then
+            mkdir -p "$workdir"
         fi
     }
 
+    function stop_systemd_resolved() {
+        printf "> Stopping systemd-resolved, the default ubuntu name resolver.\n"
+        sudo systemctl stop systemd-resolved.service 1>/dev/null
+    }
+
+    function apt_purge() {
+        app="$1"
+        if [ "$(apt list -a "$app" 2>/dev/null | grep -c 'installed')" -ge 1 ]; then
+            sudo apt-get purge --yes "$app" &>/dev/null || return 1
+        fi
+    }
+
+    function check_service_active() {
+        sudo systemctl status "$1.service" 2>/dev/null | grep 'Active: active'
+    }
+
+    function check_service_running() {
+        sudo systemctl status "$1.service" 2>/dev/null | grep '(running)'
+    }
+
+    function disable_default_dns() {
+        printf "> Disabling the default ubuntu name resolver.\n"
+
+        for s in dnsmasq systemd-resolved; do
+            if check_service_running $s; then
+                sudo systemctl stop $s.service 1>/dev/null || return 1
+            fi
+            if check_service_active $s; then
+                sudo systemctl disable $s.service 1>/dev/null || return 1
+            fi
+        done
+
+        for p in resolvctl dnsmasq; do
+            apt_purge "$p"
+        done
+    }
+
+    function expose_privileged_port_53() {
+        printf "> Setting privileges to use port 53.\n"
+        sudo sed -i.bak '/ip_unprivileged_port_start\=53/d' /etc/sysctl.conf
+        printf "net.ipv4.ip_unprivileged_port_start=53" | sudo tee -a /etc/sysctl.conf 1>/dev/null
+        sudo sysctl --system &>/dev/null
+    }
+
+    function download_docker_image() {
+        printf "> Pulling docker image %s.\n" "$image_name"
+        if [[ "$(docker images -q $image_name:latest 2>/dev/null)" == "" ]]; then
+            docker pull "$image_name:latest" 1>/dev/null || return 1
+        fi
+    }
+
+    reset
     printf "> Starting DNS setup.\n"
     sudo_advice
-    check_dependencies || return
-    ensure_workdir || return
-    local_dns_instances_running
-    remove_local_dns_instances
-    build_local_dns_image
-    # start_local_dns
-    # add_fallback_dns
-    # sleep 1
-    # test_hosts
-    printf "> DNS setup finished.\n"
+    check_dependencies || return 1
+    ensure_workdir || return 1
+    remove_local_dns_instances || return 1
+    download_docker_image || return 1
+    expose_privileged_port_53 || return 1
+    disable_default_dns || return 1
+    start_local_dns || return 1
+    add_fallback_dns || return 1
+    test_hosts || return 1
+    printf "> DNS setup finished. Enjoy!\n"
+
+    cat /etc/resolv.conf
+    printf "\n"
 }
 
 trap "echo; exit" INT
