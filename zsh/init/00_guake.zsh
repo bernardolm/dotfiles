03b62b50005db01fdcc13e212bcc0c226b9e42ad
Bernardo Loureiro <bernardo.lou@gmail.com>
2023-06-19 05:03:41 -0300

    Updating guake configs

--- zsh/init/00_guake.zsh
+++ zsh/init/00_guake.zsh
@@ -1,5 +1,5 @@
 if [[ $(ps -eo comm | grep -c guake) -gt 0 ]]; then
     notify-send "Guake" "Loading personal preferences..."
-    dconf reset -f /apps/guake/
-    dconf load /apps/guake/ < "${DOTFILES}/guake/config.dconf"
+    # dconf reset -f /apps/guake/
+    # dconf load /apps/guake/ < "${DOTFILES}/guake/config.dconf"
 fi
