79788e7f2d4ae5e58908b26c3995155c860ec34c
Bernardo Loureiro <bernardo.lou@gmail.com>
2023-06-20 09:39:40 -0300

    Updating functions

--- zsh/functions/crontab.zsh
+++
@@ -1,6 +0,0 @@
-function crontab_backup() {
-    mkdir -p $SYNC_DOTFILES/crontab/
-    [ -f $SYNC_DOTFILES/crontab/$(hostname)_current.txt ] && mv \
-        $SYNC_DOTFILES/crontab/$(hostname)_current.txt $SYNC_DOTFILES/crontab/$(hostname)_$(date +"%Y%m%d%H%M%S").txt
-    crontab -l > $SYNC_DOTFILES/crontab/$(hostname)_current.txt
-}
