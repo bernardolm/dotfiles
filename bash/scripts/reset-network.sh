00b4b6d525ecf215543d5111097a085f0e4e3a48
Bernardo Loureiro <bernardo.lou@gmail.com>
2023-10-17 12:05:30 -0300

    Adding network script

---
+++ bash/scripts/reset-network.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+sudo dhclient -r || true
+sudo iptables -F || true
+sudo iptables -F INPUT || true
+sudo iptables -F OUTPUT || true
+sudo iptables -P INPUT ACCEPT || true
+sudo iptables -P OUTPUT ACCEPT || true
+sudo ip addr flush eth0 || true
+sudo netplan apply || true
+sudo service ssh restart || true
+ping -c 5 www.docker.com | grep loss
+ping -c 5 www.ubuntu.com.br | grep loss
+ping -c 5 www.google.com | grep loss
