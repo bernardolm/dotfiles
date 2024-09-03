de1cb13a40da7c6b5b148e414267deb39b4c1023
Bernardo Loureiro <bernardo.lou@gmail.com>
2024-09-03 11:00:08 -0300

    Adding gcloud init

---
+++ zsh/init.d/60_gcloud.zsh
@@ -0,0 +1,5 @@
+# The next line updates PATH for the Google Cloud SDK.
+if [ -f "$HOME/google-cloud-sdk/path.zsh.inc" ]; then . "$HOME/google-cloud-sdk/path.zsh.inc"; fi
+
+# The next line enables shell command completion for gcloud.
+if [ -f "$HOME/google-cloud-sdk/completion.zsh.inc" ]; then . "$HOME/google-cloud-sdk/completion.zsh.inc"; fi
