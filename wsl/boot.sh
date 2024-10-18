fcb217f0b7045d63cd3b55064d5b0f88a8bb9b99
Bernardo Loureiro <bernardo.lou@gmail.com>
2024-10-18 14:37:29 -0300

    Updating some config

--- wsl/boot.sh
+++ wsl/boot.sh
@@ -3,7 +3,7 @@
 # General values
 username=bernardo
 distribution="Ubuntu-24.04"
-vhd_file_path="C:/Users/$username/ubuntu-home-64gb.vhd"
+vhd_file_path="C:/Users/${username}/ubuntu-home-64gb.vhd"
 
 # Timeout and interval in seconds
 timeout=10
@@ -13,27 +13,27 @@ interval=1
 target_drive="/dev/sdc1"
 
 # Mount point
-mount_point="/home/$username"
+mount_point="/home/${username}"
 
 # Execute mount
-/mnt/c/Users/$username/AppData/Local/Microsoft/WindowsApps/wsl.exe -d $distribution --mount --vhd $vhd_file_path --bare
+eval "/mnt/c/Users/${username}/AppData/Local/Microsoft/WindowsApps/wsl.exe -d $distribution --mount --vhd ${vhd_file_path} --bare"
 
 # Initialize elapsed time counter
 elapsed=0
 
 # Wait for the target drive to be ready
-while [ ! -e $target_drive ]; do
-    if [ $elapsed -ge $timeout ]; then
-        echo "Timed out waiting for $target_drive to be ready."
+while [ ! -e ${target_drive} ]; do
+    if [ ${elapsed} -ge ${timeout} ]; then
+        echo "Timed out waiting for ${target_drive} to be ready."
         exit 1
     fi
 
-    echo "Waiting for $target_drive to be ready..."
-    sleep $interval
+    echo "Waiting for ${target_drive} to be ready..."
+    sleep ${interval}
     elapsed=$((elapsed + interval))
 done
 
 # Mount the target drive
-mount -o defaults,permissions $target_drive $mount_point
+eval "mount -o defaults,permissions ${target_drive} ${mount_point}"
 
-mount -l | grep $target_drive
+eval "mount -l | grep ${target_drive}"
