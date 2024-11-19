0778347d91bd72b0aacd2c384bf2aae2b128df13
Bernardo Loureiro <bernardo.lou@gmail.com>
2024-11-19 20:30:13 -0300

    Updating configs

---
+++ ssh/scripts/backup-keys.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env zsh
+
+set -e
+
+function cp_from_to() {
+    echo -n "transfering from ${1}: "
+    scp -o ConnectTimeout=1 -q "${USER}@${1}:${2}" \
+        "${PWD}/${1}_id_ed25519.pub" && echo "OK" || echo "failed" && true
+}
+
+for ip in 192.168.1.{10..35}; do
+    cp_from_to "${ip}" "/home/${USER}/.ssh/id_ed25519.pub"
+done
+
+cp_from_to "192.168.1.36" "C:/Users/Bernardo/.ssh/id_ed25519.pub"
---
+++ windows/sym-links.ps1
@@ -0,0 +1,5 @@
+Remove-Item "$Env:USERPROFILE\.ssh\config" ; New-Item -ItemType SymbolicLink -Target "$Env:USERPROFILE\Dropbox\shared\home\.ssh\config" -Path "$Env:USERPROFILE\.ssh\config"
+Remove-Item "$Env:USERPROFILE\.wakatime.cfg" ; New-Item -ItemType SymbolicLink -Target "$Env:USERPROFILE\Dropbox\shared\home\.wakatime.cfg" -Path "$Env:USERPROFILE\.wakatime.cfg"
+Remove-Item "$Env:USERPROFILE\.gitconfig" ; New-Item -ItemType SymbolicLink -Target "$Env:USERPROFILE\Dropbox\windows\home\.gitconfig" -Path "$Env:USERPROFILE\.gitconfig"
+Remove-Item "$Env:USERPROFILE\.wslconfig" ; New-Item -ItemType SymbolicLink -Target "$Env:USERPROFILE\Dropbox\windows\home\.wslconfig" -Path "$Env:USERPROFILE\.wslconfig"
+Remove-Item "$Env:LocalAppData\Packages\Microsoft.WindowsTerminal_8wekyb3d8bbwe\LocalState" ; New-Item -ItemType SymbolicLink -Target "$Env:USERPROFILE\Dropbox\windows\windows-terminal" -Path "$Env:LocalAppData\Packages\Microsoft.WindowsTerminal_8wekyb3d8bbwe\LocalState"
