0079951eef18d00dbecf1d47d0a89898c2e89ea1
Bernardo Loureiro <bernardo.lou@gmail.com>
2024-12-06 16:57:52 -0300

    Updating wsl files

--- wsl/boot.sh
+++ wsl/boot.sh
@@ -1,39 +1,56 @@
 #!/usr/bin/env bash
 
-# General values
-username=bernardo
-distribution="Ubuntu-24.04"
-vhd_file_path="C:/Users/${username}/ubuntu-home-64gb.vhd"
-
-# Timeout and interval in seconds
+# Configuration
+disk_uuid=D6623F1C623F00B3
+elapsed=0
+interval=2
 timeout=10
-interval=1
-
-# Drive to check and mount
-target_drive="/dev/sdc1"
+username=bernardo
+virtual_disk_path="C:/wsl/Ubuntu-24.04/ubuntu-home-64gb.vhd"
 
 # Mount point
 mount_point="/home/${username}"
 
-# Execute mount
-eval "/mnt/c/Users/${username}/AppData/Local/Microsoft/WindowsApps/wsl.exe -d $distribution --mount --vhd ${vhd_file_path} --bare"
+# Attaching virtual disk to WSL
+function attach_vhd_to_wsl() {
+    cmd="/mnt/c/Users/${username}/AppData/Local/Microsoft/WindowsApps/wsl.exe -d $WSL_DISTRO_NAME --mount --vhd ${virtual_disk_path} --bare"
+    echo "> ${cmd}"
+    eval "${cmd} | echo already attached" 2>/dev/null
+}
 
-# Initialize elapsed time counter
-elapsed=0
+# get disk device path
+function get_disk_path() {
+    blkid --uuid ${disk_uuid}
+}
+
+function mount_home() {
+    # Umounting exist mounting point
+    cmd="umount ${mount_point}"
+    echo "> ${cmd}"
+    eval "${cmd}"
+
+    cmd="mount -o defaults,permissions $(get_disk_path) ${mount_point}"
+    echo "> ${cmd}"
+    eval "${cmd}"
+
+    # Showing mounted
+    cmd="mount -l | grep ${mount_point}"
+    echo "> ${cmd}"
+    eval "${cmd}"
+}
+
+attach_vhd_to_wsl
+
+mount_home
 
 # Wait for the target drive to be ready
-while [ ! -e ${target_drive} ]; do
+while [ "$(mount -l | grep -c ${mount_point})" -eq 0 ]; do
     if [ ${elapsed} -ge ${timeout} ]; then
-        echo "Timed out waiting for ${target_drive} to be ready."
+        echo "Timed out waiting for ${mount_point} to be ready."
         exit 1
     fi
 
-    echo "Waiting for ${target_drive} to be ready..."
+    echo "Waiting for ${mount_point} to be ready..."
     sleep ${interval}
     elapsed=$((elapsed + interval))
 done
-
-# Mount the target drive
-eval "mount -o defaults,permissions ${target_drive} ${mount_point}"
-
-eval "mount -l | grep ${target_drive}"
