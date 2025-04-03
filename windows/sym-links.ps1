227d75809639db7c22eba8620019ff53e06a5742
Bernardo Loureiro <bernardo.lou@gmail.com>
2025-04-03 17:10:19 -0300

    Updating windows sym link

--- windows/sym-links.ps1
+++ windows/sym-links.ps1
@@ -1,23 +1,27 @@
-Remove-Item "$Env:USERPROFILE\.ssh\config" ; New-Item -ItemType SymbolicLink \
-    -Target "$Env:USERPROFILE\Dropbox\shared\home\.ssh\config" \
+Remove-Item "$Env:USERPROFILE\.ssh\config" ; New-Item -ItemType SymbolicLink `
+    -Target "$Env:USERPROFILE\Dropbox\shared\home\.ssh\config" `
     -Path "$Env:USERPROFILE\.ssh\config"
 
-Remove-Item "$Env:USERPROFILE\.wakatime.cfg" ; New-Item -ItemType SymbolicLink \
-    -Target "$Env:USERPROFILE\Dropbox\shared\home\.wakatime.cfg" \
+Remove-Item "$Env:USERPROFILE\.wakatime.cfg" ; New-Item -ItemType SymbolicLink `
+    -Target "$Env:USERPROFILE\Dropbox\shared\home\.wakatime.cfg" `
     -Path "$Env:USERPROFILE\.wakatime.cfg"
 
-Remove-Item "$Env:USERPROFILE\.gitconfig" ; New-Item -ItemType SymbolicLink \
-    -Target "$Env:USERPROFILE\Dropbox\windows\home\.gitconfig" \
+Remove-Item "$Env:USERPROFILE\.gitconfig" ; New-Item -ItemType SymbolicLink `
+    -Target "$Env:USERPROFILE\Dropbox\windows\home\.gitconfig" `
     -Path "$Env:USERPROFILE\.gitconfig"
 
-Remove-Item "$Env:USERPROFILE\.wslconfig" ; New-Item -ItemType SymbolicLink \
-    -Target "$Env:USERPROFILE\Dropbox\windows\home\.wslconfig" \
+Remove-Item "$Env:USERPROFILE\.wslconfig" ; New-Item -ItemType SymbolicLink `
+    -Target "$Env:USERPROFILE\Dropbox\windows\home\.wslconfig" `
     -Path "$Env:USERPROFILE\.wslconfig"
 
-Remove-Item "$Env:LocalAppData\Packages\Microsoft.WindowsTerminal_8wekyb3d8bbwe\LocalState" ; New-Item -ItemType SymbolicLink \
-    -Target "$Env:USERPROFILE\Dropbox\windows\windows-terminal" \
+Remove-Item "$Env:LocalAppData\Packages\Microsoft.WindowsTerminal_8wekyb3d8bbwe\LocalState" ; New-Item -ItemType SymbolicLink `
+    -Target "$Env:USERPROFILE\Dropbox\windows\windows-terminal" `
     -Path "$Env:LocalAppData\Packages\Microsoft.WindowsTerminal_8wekyb3d8bbwe\LocalState"
 
-Remove-Item "$Env:USERPROFILE\Documents\PowerShell\Microsoft.PowerShell_profile.ps1" ; New-Item -ItemType SymbolicLink \
-    -Target "$Env:USERPROFILE\Dropbox\windows\pws\Microsoft.PowerShell_profile.ps1" \
+Remove-Item "$Env:USERPROFILE\Documents\PowerShell\Microsoft.PowerShell_profile.ps1" ; New-Item -ItemType SymbolicLink `
+    -Target "$Env:USERPROFILE\Dropbox\windows\pws\Microsoft.PowerShell_profile.ps1" `
     -Path "$Env:USERPROFILE\Documents\PowerShell\Microsoft.PowerShell_profile.ps1"
+
+Remove-Item "$Env:USERPROFILE\Documents\PowerShell\powershell.config.json" ; New-Item -ItemType SymbolicLink `
+    -Target "$Env:USERPROFILE\Dropbox\windows\pws\powershell.config.json" `
+    -Path "$Env:USERPROFILE\Documents\PowerShell\powershell.config.json"
