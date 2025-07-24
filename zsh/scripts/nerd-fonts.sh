230feb5cb1b6d9713ddfe6d8310ad12c02659ad2
Bernardo Loureiro <bernardo.lou@gmail.com>
2025-07-24 20:56:04 -0300

    Update paths in configuration files for consistency and clarity

--- zsh/scripts/nerd-fonts.sh
+++ zsh/scripts/nerd-fonts.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env zsh
 
-docker run --rm -v "$HOME/shared/fonts/NovaMono:/in:Z" -v "$HOME/shared/fonts/patched:/out:Z" -e "PN=10" nerdfonts/patcher -c -ext ttf
+docker run --rm -v "$HOME/sync/shared/fonts/NovaMono:/in:Z" -v "$HOME/sync/shared/fonts/patched:/out:Z" -e "PN=10" nerdfonts/patcher -c -ext ttf
