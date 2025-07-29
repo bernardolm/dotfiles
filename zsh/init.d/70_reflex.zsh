9130a650dc01fcefd9f0e916428dc73a662a38fd
Bernardo Loureiro <bernardo.lou@gmail.com>
2025-07-29 09:31:18 -0300

    Adding reflex deps to init script

---
+++ zsh/init.d/70_reflex.zsh
@@ -0,0 +1,8 @@
+## Reflex
+
+# bun completions
+[ -s "/home/bernardo/.local/share/reflex/bun/_bun" ] && source "/home/bernardo/.local/share/reflex/bun/_bun"
+
+# bun
+export BUN_INSTALL="$HOME/.local/share/reflex/bun"
+export PATH="$BUN_INSTALL/bin:$PATH"
