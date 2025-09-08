262cc48a64ea76e6a4172867b5236ff60513b60b
Bernardo Loureiro <bernardo.lou@gmail.com>
2025-09-08 12:14:42 -0300

    Updating zsh init scripts

---
+++ zsh/init.d/atuin.zsh
@@ -0,0 +1,12 @@
+export ATUIN_NOBIND=true
+. "$HOME/.atuin/bin/env"
+eval "$(atuin init zsh)"
+
+bindkey "^R" atuin-search
+
+# incremental history search with arrow keys
+bindkey "^[[A" history-beginning-search-backward
+bindkey "^[[B" history-beginning-search-forward]]
+
+atuin import auto &>/dev/null
+atuin sync &>/dev/null
---
+++ zsh/init.d/eza.zsh
@@ -0,0 +1,3 @@
+alias ls='eza --icons --color=auto --group-directories-first'
+alias ll='eza -l --icons --color=auto --group-directories-first'
+alias la='eza -la --icons --color=auto --group-directories-first'
