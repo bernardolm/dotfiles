bf65041fa937eaf7f0aac2134182fed925b53139
Bernardo Loureiro <bernardo.lou@gmail.com>
2025-09-26 14:46:38 -0300

    Fixing atuin settings

--- zsh/init.d/atuin.zsh
+++ zsh/init.d/atuin.zsh
@@ -1,6 +1,6 @@
 if [ ! $(atuin account verify &>/dev/null) ]; then
-	atuin login -u $GITHUB_USER 2>/dev/null
-	atuin sync &>/dev/null
+	atuin login -u $GITHUB_USER 1>/dev/null
+	atuin sync 1>/dev/null
 else
-	atuin daemon
+	atuin daemon 1>/dev/null &
 fi
