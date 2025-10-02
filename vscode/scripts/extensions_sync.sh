c6667198084c838c3eb6d147a9074f530e1b6e0b
Bernardo Loureiro <bernardo.lou@gmail.com>
2025-10-02 10:16:15 -0300

    Improving code's extensions sync

--- vscode/scripts/extensions_sync.sh
+++ vscode/scripts/extensions_sync.sh
@@ -7,7 +7,9 @@ total=$(jq '.recommendations | length' $HOME/sync/windows/home/AppData/Roaming/C
 count=0
 
 source $DOTFILES/zsh/functions/now
-log="$HOME/tmp/code_extensions_sync.$(now).log"
+log="$USER_TMP/code_extensions_sync.$(now).log"
+
+echo "> logging into $log"
 
 source $DOTFILES/zsh/functions/progress_bar
 
@@ -15,18 +17,28 @@ jq -r '.recommendations[]' $HOME/sync/windows/home/AppData/Roaming/Code/User/ext
 	while read ext ; do
 		count=$((count+1))
 
-		text="> "
+		echo -e "\n\n_ $ext\n" >> $log
+
+		text=""
 
 		if [[ "$ext" =~ ^"-" ]] ; then
 			ext=${ext:1}
 			text+="removing"
-			code_cli --uninstall-extension $ext >> $output || true;
+			code_cli --log error \
+				--extensions-dir $HOME/.vscode-server/extensions \
+				--user-data-dir $HOME/.vscode-server/data \
+				--uninstall-extension $ext &>> $log || true;
 		else
 			text+="adding"
-			code_cli --install-extension $ext >> $output || true;
+			code_cli --log error \
+				--extensions-dir $HOME/.vscode-server/extensions \
+				--user-data-dir $HOME/.vscode-server/data \
+				--install-extension $ext &>> $log || true;
 		fi
 
 		text+=" $ext"
 
 		progress_bar $count $total "$text"
 	done
+
+cat $log
