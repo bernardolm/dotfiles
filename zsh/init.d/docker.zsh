bd90cb7a47b5e89003bf15f453b164efb02c9d47
Bernardo Loureiro <bernardo.lou@gmail.com>
2025-10-08 16:25:25 -0300

    Adding docker complerions

---
+++ zsh/init.d/docker.zsh
@@ -0,0 +1,5 @@
+# The following lines have been added by Docker Desktop to enable Docker CLI completions.
+fpath=("HOME/.docker/completions" $fpath)
+autoload -Uz compinit
+compinit
+# End of Docker CLI completions
