1422f2806807445e5e2ec20a2ca5975ba687e8e4
Bernardo Loureiro <bernardo.lou@gmail.com>
2025-11-17 13:45:28 -0300

    Updating file perms

--- vscode/scripts/workspace.sh
+++ vscode/scripts/workspace.sh
@@ -1,4 +1,10 @@
 #!/bin/sh
 
+if [ "$OS" = "windows" ]; then
+	echo "> I don't know what to do :("
+	echo "> maybe use sync/bin/envsubst.exe"
+	exit 0
+fi
+
 envsubst < ~/sync/shared/vs.code-workspace > ~/sync/$OS/vs.code-workspace
 code ~/sync/$OS/vs.code-workspace
