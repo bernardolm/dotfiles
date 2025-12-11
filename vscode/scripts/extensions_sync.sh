7ca4cd719cb23f9c29e54e39d77860a85954fb6a
Bernardo Loureiro <bernardo.lou@gmail.com>
2025-12-10 22:22:12 -0300

    Fixing vscode extensions manager

--- vscode/scripts/extensions_sync.sh
+++ vscode/scripts/extensions_sync.sh
@@ -1,21 +1,26 @@
 #!/usr/bin/env /bin/zsh
 
-[[ -z "$(uname)" ]] && echo "> this script willn't works on windows" && exit 0
+source "$DOTFILES/zsh/functions/now"
+source "$DOTFILES/zsh/functions/progress_bar"
 
-command -v code_cli &>/dev/null || echo "code cli not found, sym linking... " && \
-	ln -sf $HOME/sync/shared/vscode/code_cli $HOME/.local/bin/code_cli
+zsh --no-rcs "$DOTFILES/zsh/scripts/os-resolver.sh"
 
-total=$(jq '.recommendations | length' $HOME/sync/windows/home/AppData/Roaming/Code/User/extensions.json)
+echo "> os: $OS"
+echo "> home: $HOME"
+
+[[ -z "$OS" ]] && echo "> os unknown" && exit 0
+
+([[ "$OS" = "windows" ]] || [[ "$OS" = "wsl" ]]) \
+	&& echo "> this script willn't works on windows" && exit 0
+
+total=$(/bin/cat $DOTFILES/vscode/extensions.json | grep -v '//' | jq '. | length')
 count=0
 
-source $DOTFILES/zsh/functions/now
 log="$TMP_USER/code_extensions_sync.$(now).log"
 
 echo "> logging into $log"
 
-source $DOTFILES/zsh/functions/progress_bar
-
-jq -r '.recommendations[]' $HOME/sync/windows/home/AppData/Roaming/Code/User/extensions.json | \
+/bin/cat $DOTFILES/vscode/extensions.json | grep -v '//' | jq -r '.[]' | \
 	while read ext ; do
 		count=$((count+1))
 
@@ -25,28 +30,29 @@ jq -r '.recommendations[]' $HOME/sync/windows/home/AppData/Roaming/Code/User/ext
 
 		if [[ "$ext" =~ ^"-" ]] ; then
 			ext=${ext:1}
-			text+="removing"
-			code_cli \
-				--extensions-dir $HOME/.vscode-server/extensions \
+			text=-
+				# --extensions-dir $HOME/.vscode-server/extensions \
+				# --user-data-dir $HOME/.vscode-server/data \
+			code-cli \
 				--force \
 				--log error \
 				--uninstall-extension $ext \
-				--user-data-dir $HOME/.vscode-server/data \
 				&>> $log || true ;
 		else
-			text+="adding"
-			code_cli \
-				--extensions-dir $HOME/.vscode-server/extensions \
+			text=+
+				# --extensions-dir $HOME/.vscode-server/extensions \
+				# --user-data-dir $HOME/.vscode-server/data \
+			code-cli \
 				--force \
 				--install-extension $ext \
 				--log error \
-				--user-data-dir $HOME/.vscode-server/data \
 				&>> $log || true ;
 		fi
 
 		text+=" $ext"
 
-		progress_bar $count $total "$text"
+		echo -e "> $count\t$total\t$text"
+		# progress_bar $count $total "$text"
 	done
 
 cat $log
