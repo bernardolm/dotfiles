0def3722de8dec77f7687cdd608b25e9266ff95e
Bernardo Loureiro <bernardo.lou@gmail.com>
2026-02-13 19:47:37 -0300

    refactor: remove exa references and standardize aliases to use eza

--- terminal/zsh/zshrc.d/aliases.zsh
+++ terminal/zsh/zshrc.d/aliases.zsh
@@ -1,7 +1,6 @@
 alias code-server='$HOME/bin/code serve-web --accept-server-license-terms --port 8000'
 
-alias exa=eza
 alias frm='rm -rf'
 alias la='eza -lah'
 alias ll='eza -lh'
-alias ls=exa
+alias ls=eza
