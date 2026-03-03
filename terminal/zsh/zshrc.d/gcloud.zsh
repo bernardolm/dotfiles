16f9ff070f40cd628391f49319f8876c2e9f09ca
Bernardo Loureiro <bernardo.lou@gmail.com>
2026-03-03 18:15:54 -0300

    Add VS Code profile aliases and gcloud zsh setup

---
+++ terminal/zsh/zshrc.d/gcloud.zsh
@@ -0,0 +1,8 @@
+
+# shell-required: This file is sourced by zsh at startup to load Google Cloud SDK PATH and completion integrations.
+
+# The next line updates PATH for the Google Cloud SDK.
+if [ -f "$HOME/google-cloud-sdk/path.zsh.inc" ]; then . "$HOME/google-cloud-sdk/path.zsh.inc"; fi
+
+# The next line enables shell command completion for gcloud.
+if [ -f "$HOME/google-cloud-sdk/completion.zsh.inc" ]; then . "$HOME/google-cloud-sdk/completion.zsh.inc"; fi
