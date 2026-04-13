4964ebbd521398b6e6461cbfe9e1e4733b9192b7
Bernardo Loureiro <bernardo.lou@gmail.com>
2026-04-13 13:44:55 -0300

    Fixing compinit

--- cli/zsh/zshrc.d/docker.zsh
+++ cli/zsh/zshrc.d/docker.zsh
@@ -4,9 +4,13 @@ if command -v docker >/dev/null 2>&1; then
 	alias di='docker images'
 fi
 
+# The following lines have been added by Docker Desktop to enable Docker CLI completions.
 if [ -d "$HOME/.docker/completions" ]; then
 	fpath=("$HOME/.docker/completions" $fpath)
+	# autoload -Uz compinit
+	# compinit
 fi
+# End of Docker CLI completions
 
 if [ -d "$HOME/.docker/bin" ] && [[ ":$PATH:" != *":$HOME/.docker/bin:"* ]]; then
 	export PATH="$PATH:$HOME/.docker/bin"
