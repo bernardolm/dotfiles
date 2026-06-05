d2efcef2db29389b16bae6e55b4d2e304ca2621b
Bernardo Loureiro <bernardo.lou@gmail.com>
2026-06-05 18:12:55 -0300

    Updating cli config

---
+++ cli/scripts/ip.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env /bin/sh
+
+set -e
+set -x
+set -v
+
+filepath="$HOME/sync/ip_log.txt"
+[ ! -f "$filepath" ] && touch "$filepath"
+
+ip_public=$(curl -s ifconfig.me)
+ip_local=$(hostname -I 2>/dev/null || \
+	ipconfig getifaddr en1 || \
+	ipconfig getifaddr en0)
+
+now=$(date +"%Y-%m-%d %H:%M:%S")
+host=$(hostname -s)
+
+printf \
+	'%s\n' '0i' "$now $ip_local $ip_public $host" \
+	'.' 'w' 'q' | \
+	ed -s $filepath
+
+head -n5 $filepath
