6a97c93ae4f711707d4b1407f6d0fcdceeb24b9a
Bernardo Loureiro <bernardo.lou@gmail.com>
2026-07-23 14:43:20 -0300

    Updating start up scripts

---
+++ cli/zsh/zshrc.d/50_ai.zsh
@@ -0,0 +1,3 @@
+export OPENAI_API_KEY=none
+export OPENAI_BASE_URL=http://127.0.0.1:8080/v1
+export OPENAI_MODEL=mlx-community/Qwen3-14B-4bit
--- cli/zsh/zshrc.d/50_homebrew.zsh
+++ cli/zsh/zshrc.d/50_homebrew.zsh
@@ -1,13 +1,24 @@
 [[ "$DOTFILES_OS" != "darwin" ]] && return
 
-export CPPFLAGS="-I/opt/homebrew/opt/libpq/include"
+export CPPFLAGS="-I /opt/homebrew/opt/curl/include $CPPFLAGS"
+export CPPFLAGS="-I /opt/homebrew/opt/libpq/include $CPPFLAGS"
+export CPPFLAGS="-I /opt/homebrew/opt/mysql-client/include $CPPFLAGS"
+export CPPFLAGS="-I /opt/homebrew/opt/node@22/include $CPPFLAGS"
 export HOMEBREW_DISPLAY_INSTALL_TIMES=true
 export HOMEBREW_NO_ENV_HINTS=1
 export HOMEBREW_NO_REQUIRE_TAP_TRUST=1
 export HOMEBREW_UPGRADE_GREEDY=true
-export LDFLAGS="-L /opt/homebrew/opt/libpq/lib"
+export LDFLAGS="-L /opt/homebrew/opt/curl/lib $LDFLAGS"
+export LDFLAGS="-L /opt/homebrew/opt/libpq/lib $LDFLAGS"
+export LDFLAGS="-L /opt/homebrew/opt/mysql-client/lib $LDFLAGS"
+export LDFLAGS="-L /opt/homebrew/opt/node@22/lib $LDFLAGS"
 export PATH="/opt/homebrew/bin:$PATH"
+export PATH="/opt/homebrew/opt/curl/bin:$PATH"
 export PATH="/opt/homebrew/opt/libpq/bin:$PATH"
+export PATH="/opt/homebrew/opt/mysql-client/bin:$PATH"
 export PATH="/opt/homebrew/opt/node@22/bin:$PATH"
+export PATH="/opt/homebrew/sbin:$PATH"
 export PATH="/opt/homebrew/share/google-cloud-sdk/bin:$PATH"
-export PKG_CONFIG_PATH="/opt/homebrew/opt/libpq/lib/pkgconfig"
+export PKG_CONFIG_PATH=/opt/homebrew/opt/libpq/lib/pkgconfig
+
+eval "$(/opt/homebrew/bin/brew shellenv zsh)"
