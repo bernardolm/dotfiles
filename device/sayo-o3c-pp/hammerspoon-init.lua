971245c6b76a85f544e1c733c061cf3996d10dca
Bernardo Loureiro <bernardo.lou@gmail.com>
2026-09-09 18:12:37 -0300

    style(hammerspoon): format hotkey bindings

--- device/sayo-o3c-pp/hammerspoon-init.lua
+++ device/sayo-o3c-pp/hammerspoon-init.lua
@@ -1,5 +1,5 @@
-hs.hotkey.bind({"cmd"}, "`", function()
-  -- hs.application.launchOrFocus("Wezterm")
+hs.hotkey.bind({ "cmd" }, "`", function()
+	-- hs.application.launchOrFocus("Wezterm")
 
 	wez = hs.application.find("Wezterm")
 
@@ -14,19 +14,18 @@ end)
 
 -- SayoDevice O3C macro keys (Z/X/C/V -> ctrl+alt+cmd+1/2/3/4)
 -- F13/F14/F15 were reserved by macOS for brightness, so we moved to this chord.
-hs.hotkey.bind({"ctrl", "alt", "cmd"}, "1", function()
+hs.hotkey.bind({ "ctrl", "alt", "cmd" }, "1", function()
 	hs.application.launchOrFocus("/Applications/Docker.app/Contents/MacOS/Docker Desktop.app")
 end)
 
-hs.hotkey.bind({"ctrl", "alt", "cmd"}, "2", function()
+hs.hotkey.bind({ "ctrl", "alt", "cmd" }, "2", function()
 	hs.application.launchOrFocus("Wezterm")
 end)
 
-hs.hotkey.bind({"ctrl", "alt", "cmd"}, "3", function()
+hs.hotkey.bind({ "ctrl", "alt", "cmd" }, "3", function()
 	hs.execute("/Users/bernardo/sync/personal/vpn/acao/openvpn.sh &")
 end)
 
-hs.hotkey.bind({"ctrl", "alt", "cmd"}, "4", function()
+hs.hotkey.bind({ "ctrl", "alt", "cmd" }, "4", function()
 	hs.application.launchOrFocus("Claude")
 end)
-
